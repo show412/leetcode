0267fb8f7444f8c5a9aa5bd197036e7b834b0717
hwsun <hwsun@freewheel.tv>
2018-08-16 15:07:39 +0800

    https://leetcode.com/problems/longest-palindromic-substring/description/

--- problems/medium/Longest_Palindromic_Substring.rb
+++ problems/medium/Longest_Palindromic_Substring.rb
@@ -17,16 +17,69 @@ def longest_palindrome(s)
   r_s = s.reverse
   length = r_s.length
   i=0
-  j=2
+  j=1
   while (i<length) do
-    break if (j+i > length)
+    break if (i+j>length)
     idx = s.index(r_s.slice(i,j))
     if !idx.nil?
-      longest_palindro = s.slice(idx,j)
-      j += 1
+      if check_palindro(s.slice(idx,j))
+        longest_palindro = s.slice(idx,j)
+        j += 1
+      else
+        i += 1
+        j += 1
+      end
     else
       i += 1
     end
   end
   return longest_palindro
+end
+
+def check_palindro(s)
+  length1 = s.length
+  i1 = 0
+  j1 = length1-1
+  flag = true
+  while(i1<length1)
+    break if i1 == j1 || j1 <0 || i1>j1
+    if s[i1] != s[j1]
+      flag = false
+      break
+    else
+      i1 +=1
+      j1 -=1
+    end
+  end
+  return flag
+end
+
+
+# The best solution
+def longest_palindrome(s)
+    max_indexes = [0, 0]
+
+    (0..(s.size - 1)).each do |idx|
+        start_1, end_1 = expand_palindrome(idx, idx, s)
+        start_2, end_2 = expand_palindrome(idx, idx + 1, s)
+
+        if end_1 - start_1 > max_indexes[1] - max_indexes[0]
+            max_indexes = [start_1, end_1]
+        end
+
+        if end_2 - start_2 > max_indexes[1] - max_indexes[0]
+            max_indexes = [start_2, end_2]
+        end
+    end
+
+    s[max_indexes[0]..max_indexes[1]]
+end
+
+def expand_palindrome(start_idx, end_idx, s)
+    while start_idx >= 0 && end_idx <= s.size - 1 && s[start_idx] == s[end_idx]
+        start_idx -= 1
+        end_idx += 1
+    end
+
+    return start_idx + 1, end_idx - 1
 end
