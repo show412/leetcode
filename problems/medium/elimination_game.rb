8542fa43484f7e09606bfc5c69f6011480e75837
hwsun <hwsun@freewheel.tv>
2018-08-31 19:37:24 +0800

    https://leetcode.com/problems/elimination-game/description/

---
+++ problems/medium/elimination_game.rb
@@ -0,0 +1,41 @@
+# There is a list of sorted integers from 1 to n. Starting from left to right, remove the first number and every other number afterward until you reach the end of the list.
+
+# Repeat the previous step again, but this time from right to left, remove the right most number and every other number from the remaining numbers.
+
+# We keep repeating the steps again, alternating left to right and right to left, until a single number remains.
+
+# Find the last number that remains starting with a list of length n.
+
+# Example:
+
+# Input:
+# n = 9,
+# 1 2 3 4 5 6 7 8 9
+# 2 4 6 8
+# 2 6
+# 6
+
+# Output:
+# 6
+# https://leetcode.com/problems/elimination-game/description/
+
+# @param {Integer} n
+# @return {Integer}
+def last_remaining(n)
+  left = true;
+  remaining = n;
+  step = 1;
+  head = 1;
+  while (remaining > 1) do
+#       only from left or the remaining is odd when move from right, the head will move to next posion
+      if (left || remaining % 2 ==1)
+          head = head + step;
+      end
+#     the left length is half of current length whatever the n is even or odd
+      remaining = remaining / 2;
+#     step will be double after moving evey time
+      step = step * 2;
+      left = !left;
+  end
+  return head;
+end
