514dd458b3a8b78daa377f0a7945e704857bb7c8
hwsun <hwsun@freewheel.tv>
2018-09-11 19:39:18 +0800

    https://leetcode.com/problems/zigzag-conversion/description/

---
+++ problems/medium/zigzag_conversion.rb
@@ -0,0 +1,67 @@
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string s, int numRows);
+Example 1:
+
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
+Example 2:
+
+Input: s = "PAYPALISHIRING", numRows = 4
+Output: "PINALSIGYAHRPI"
+Explanation:
+
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+https://leetcode.com/problems/zigzag-conversion/description/
+
+
+# @param {String} s
+# @param {Integer} num_rows
+# @return {String}
+def convert(s, num_rows)
+  length = s.length
+  i = 0
+  c_array = []
+  s_array = []
+  cs=""
+  while( i< length) do
+    mod = c_array.length%num_rows
+    if mod == 0
+      s_array = []
+      s_array << s[i] 
+    else
+      j = 0
+      while(j<num_rows) do
+        if j == num_rows - num
+          s_array << s[i]
+        else
+          s_array << ""
+        end
+        j+=1
+      end
+    end
+    c_array << s_array if s_array.length % num_rows == 0
+    i+=1  
+  end
+  
+  j=0
+  while(j<num_rows) do
+    i = 0
+    while(i<c_array.length) do 
+      cs += c_array[i][j]
+      i+=1
+    end
+    j +=1
+  end
+  cs
+end
