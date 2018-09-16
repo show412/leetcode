3abd362d75c3359d7f8df315404b859dbcafacbd
hwsun <hwsun@freewheel.tv>
2018-09-16 23:26:43 +0800

    https://leetcode.com/problems/subsets/description/

---
+++ problems/medium/subset.rb
@@ -0,0 +1,40 @@
+# Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+# Note: The solution set must not contain duplicate subsets.
+
+# Example:
+
+# Input: nums = [1,2,3]
+# Output:
+# [
+#   [3],
+#   [1],
+#   [2],
+#   [1,2,3],
+#   [1,3],
+#   [2,3],
+#   [1,2],
+#   []
+# ]
+
+# @param {Integer[]} nums
+# @return {Integer[][]}
+
+def subsets(nums)
+  path = []
+  @result = []
+  nums.sort!
+  subsetsHelper(path, nums, 0, @result)
+  @result
+end
+
+def subsetsHelper(path, nums, pos, result)
+  result << path
+  i = pos
+  while(i < nums.length)
+    path << nums[i]
+    subsetsHelper(path, nums, i + 1, result)
+    path.pop
+    i += 1
+  end
+end
