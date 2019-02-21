553b4511cbf354c728b312e5b91cca044850ca40
hwsun <hwsun@freewheel.tv>
2019-02-21 17:03:09 +0800

    https://leetcode.com/problems/triangle/submissions/

---
+++ problems/medium/triangle_DP.rb
@@ -0,0 +1,26 @@
+# @param {Integer[][]} triangle
+# @return {Integer}
+def minimum_total(triangle)
+  #   f is used to set the min path from botom to top
+    i=0
+    f = []
+    while (i<triangle.length)
+      f.push({})
+      i += 1
+    end
+    i = 0
+    while (i<triangle.length)
+      f[triangle.length - 1][i] = triangle[triangle.length - 1][i]
+      i += 1
+    end
+    i = triangle.length - 2
+    while (i>=0)
+      j = 0
+      while(j<=i)
+        f[i][j] = [f[i+1][j],f[i+1][j+1]].min + triangle[i][j]
+        j += 1
+      end
+      i -= 1
+    end
+    f[0][0]  
+  end
