101882d898808a4fda26b961828dfa2ce2dfc38b
hwsun <hwsun@freewheel.tv>
2019-04-02 22:37:03 +0800

    https://leetcode.com/problems/triangle/ improve add some comments

--- problems/medium/triangle_DP.rb
+++ problems/medium/triangle_DP.rb
@@ -4,15 +4,18 @@ def minimum_total(triangle)
   #   f is used to set the min path from botom to top
     i=0
     f = []
+    # state: f stands that the min path from i to j
     while (i<triangle.length)
       f.push({})
       i += 1
     end
+    # initalize: for the last level the min path
     i = 0
     while (i<triangle.length)
       f[triangle.length - 1][i] = triangle[triangle.length - 1][i]
       i += 1
     end
+    # func: from bottom to top
     i = triangle.length - 2
     while (i>=0)
       j = 0
@@ -22,5 +25,5 @@ def minimum_total(triangle)
       end
       i -= 1
     end
-    f[0][0]  
+    f[0][0]
   end
