96cbbdb6d7c61cb32a613b286005d2ebd510c3e1
hwsun <hwsun@freewheel.tv>
2019-04-10 23:01:26 +0800

    https://leetcode.com/problems/majority-element-ii/submissions/

--- problems/medium/majority_elementII.rb
+++ problems/medium/majority_elementII.rb
@@ -23,3 +23,39 @@ def majority_element(nums)
   end
   result
 end
+
+# time complexity is O(n) space complexity is O(1)
+def majority_element(nums)
+  return [] if nums.nil? || nums.empty?
+
+  length = nums.length
+  i = 0
+  candidate1 = nil
+  candidate2 = nil
+  count1 = 0
+  count2 = 0
+  while(i<length)
+    if(nums[i] == candidate1)
+      count1 += 1
+    elsif(nums[i] == candidate2)
+      count2 += 1
+    elsif(count1 == 0)
+      candidate1 = nums[i]
+    elsif(count2 == 0)
+      candidate2 = nums[i]
+    else
+      count1 -= 1
+      count2 -= 1
+    end
+    i += 1
+  end
+  count1 = 0
+  count2 = 0
+  i = 0
+  while(i<length)
+    count1 += 1 if(nums[i] == candidate1)
+    count2 += 1 if(nums[i] == candidate2)
+    i += 1
+  end
+  count1 > count2 ? candidate1 : candidate2
+end
