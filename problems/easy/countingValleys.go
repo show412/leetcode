c2d3937c96c2adcee2edc06d692826f90ad3f9d3
hwsun <hwsun@freewheel.tv>
2019-04-18 00:13:21 +0800

    未完 待做

---
+++ problems/easy/countingValleys.go
@@ -0,0 +1,11 @@
+// Complete the countingValleys function below.
+// https://www.hackerrank.com/challenges/counting-valleys/problem
+// 遍历这个数组看看有没有和是负数的情况总数
+func countingValleys(n int32, s string) int32 {
+	var count int32
+	var i int32
+	for i = 0; i < n; i++ {
+
+	}
+
+}
