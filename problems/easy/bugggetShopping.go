fa26f45540b62beebf17be564fcae41d0387482f
hwsun <hwsun@freewheel.tv>
2019-04-23 23:15:29 +0800

    https://leetcode.com/problems/max-chunks-to-make-sorted-ii/submissions/

--- problems/easy/bugggetShopping.go
+++ problems/easy/bugggetShopping.go
@@ -8,12 +8,7 @@ func budgetShopping(n int32, bundleQuantities []int32, bundleCosts []int32) int3
 				f[i][j] = false
 			}
 		}
-		boolean f[][] = new boolean[A.length + 1][m + 1];
-        for (int i = 0; i <= A.length; i++) {
-            for (int j = 0; j <= m; j++) {
-                f[i][j] = false;
-            }
-        }
+
 				f[0][0] = true;
 				for i := 1; i<=len(bundleQuantities) ; i++ {
 					for j:=0; j<=n; j++ {
@@ -23,14 +18,7 @@ func budgetShopping(n int32, bundleQuantities []int32, bundleCosts []int32) int3
                 }
 					}
 				}
-        for (int i = 1; i <= A.length; i++) {
-            for (int j = 0; j <= m; j++) {
-                f[i][j] = f[i - 1][j];
-                if (j >= A[i-1] && f[i-1][j - A[i-1]]) {
-                    f[i][j] = true;
-                }
-            } // for j
-        } // for i
+
 
         for (int i = m; i >= 0; i--) {
             if (f[A.length][i]) {
---
+++ problems/hard/maxChunkToSort.go
@@ -0,0 +1,47 @@
+// https://leetcode.com/problems/max-chunks-to-make-sorted-ii/
+// if there could be chunk, the max number in last chunk should be
+// smaller than the min number in next chunk
+// so we could use the thought to resolve the question
+// Basiclly, it's a DP
+func maxChunksToSorted(arr []int) int {
+	var chunkNumber int = 0
+	var length int = len(arr)
+	//DP state
+	// the last chunk slice, lastChunk[i] means the max number in first i numbers
+	lastChunk := make([]int, length)
+	// the next chunk slice,nextChunk[i] means the min number in next i numbers
+	nextChunk := make([]int, length)
+	//DP init
+	lastChunk[0] = 0
+	nextChunk[length-1] = arr[length-1]
+	// DP function
+	// init the lastChunk
+	for i := 1; i < len(arr); i++ {
+		lastChunk[i] = max(arr[i], lastChunk[i-1])
+	}
+	// init the nextChunk
+	for i := len(arr) - 2; i >= 0; i-- {
+		nextChunk[i] = min(arr[i], nextChunk[i+1])
+	}
+	// result
+	for i := 0; i < len(arr)-1; i++ {
+		if lastChunk[i] <= nextChunk[i+1] {
+			chunkNumber++
+		}
+	}
+	return chunkNumber + 1
+}
+func max(a int, b int) int {
+	if a < b {
+		return b
+	} else {
+		return a
+	}
+}
+func min(a int, b int) int {
+	if a > b {
+		return b
+	} else {
+		return a
+	}
+}
