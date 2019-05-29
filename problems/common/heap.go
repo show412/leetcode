9d832d6435c648e67aa562d77e8f33764edef988
hwsun <hwsun@freewheel.tv>
2019-05-29 18:52:55 +0800

    heap with golang and kth largest with quick select

---
+++ problems/common/heap.go
@@ -0,0 +1,37 @@
+import "fmt"
+
+// 构建最小堆
+func minHeap(root int, end int, c []int) {
+	for {
+		var child = 2*root + 1
+		//判断是否存在child节点
+		if child > end {
+			break
+		}
+		//判断右child是否存在，如果存在则和另外一个同级节点进行比较
+		if child+1 <= end && c[child] > c[child+1] {
+			child += 1
+		}
+		if c[root] > c[child] {
+			c[root], c[child] = c[child], c[root]
+			root = child
+		} else {
+			break
+		}
+	}
+}
+
+//降序排序
+func HeapSort(c []int) {
+	var n = len(c) - 1
+	for root := n / 2; root >= 0; root-- {
+		minHeap(root, n, c)
+	}
+	fmt.Println("堆构建完成")
+	for end := n; end >= 0; end-- {
+		if c[0] < c[end] {
+			c[0], c[end] = c[end], c[0]
+			minHeap(0, end-1, c)
+		}
+	}
+}
---
+++ problems/google/201902OA/kth_largest.go
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/kth-largest-element-in-an-array/
+// refer to https://www.jianshu.com/p/c9664734033e
+// 可以直接排序 O(NlogN) O(1)
+// 可以构建最小堆排序 也可以构建最大堆排序 refer to https://blog.csdn.net/mofiu/article/details/83620743
+// O(Nlogk) O(k)
+// 也可以用 quickSelect 算法 O(N) in the average case, {O}(N^2)in the worst case.
+// space complexity is O(1)
+func findKthLargest(nums []int, k int) int {
+
+}
