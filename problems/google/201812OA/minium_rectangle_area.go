9d8440c71681a04125b9a2502fd571b57d3f0e6f
hwsun <hwsun@freewheel.tv>
2019-06-03 23:29:27 +0800

    min area rectangle solution

--- problems/google/201812OA/minium_rectangle_area.go
+++ problems/google/201812OA/minium_rectangle_area.go
@@ -17,6 +17,7 @@ import (
 // Input: [[1,1],[1,3],[3,1],[3,3],[4,1],[4,3]]
 // Output: 2
 // https://leetcode.com/problems/minimum-area-rectangle/
+// this solution is time limitted exceed
 func minAreaRect(points [][]int) int {
 	if len(points) < 4 {
 		return 0
@@ -39,7 +40,10 @@ func minAreaRect(points [][]int) int {
 
 func dfs(points *[][]int, subPoints *[][]int, start int, res *[]int) {
 	if len(*subPoints) == 4 {
-		area := areaRect(*subPoints)
+		// areaRect is for all possible point to combine a rect
+		// area := areaRect(*subPoints)
+		// parollAreaRect is for those x,y is paroll to x-axis and y-axis
+		area := parollAreaRect(*subPoints)
 		if area > 0 {
 			*res = append(*res, area)
 			return
@@ -56,6 +60,37 @@ func dfs(points *[][]int, subPoints *[][]int, start int, res *[]int) {
 	return
 }
 
+func parollAreaRect(subPoints [][]int) int {
+	point1_x := subPoints[0][0]
+	point1_y := subPoints[0][1]
+	point2_x := subPoints[1][0]
+	point2_y := subPoints[1][1]
+	point3_x := subPoints[2][0]
+	point3_y := subPoints[2][1]
+	point4_x := subPoints[3][0]
+	point4_y := subPoints[3][1]
+	if !((point1_x == point2_x && point3_x == point4_x) ||
+		(point1_x == point3_x && point2_x == point4_x) ||
+		(point1_x == point4_x && point2_x == point3_x)) == true {
+		return 0
+	}
+	if !((point1_y == point2_y && point3_y == point4_y) ||
+		(point1_y == point3_y && point2_y == point4_y) ||
+		(point1_y == point4_y && point2_y == point3_y)) == true {
+		return 0
+	}
+	if point1_x == point2_x {
+		return int(math.Abs(float64((point2_y - point1_y) * (point3_x - point1_x))))
+	}
+	if point1_x == point3_x {
+		return int(math.Abs(float64((point3_y - point1_y) * (point2_x - point1_x))))
+	}
+	if point1_x == point4_x {
+		return int(math.Abs(float64((point4_y - point1_y) * (point3_x - point1_x))))
+	}
+	return 0
+}
+
 func areaRect(subPoints [][]int) int {
 	point1_x := subPoints[0][0]
 	point1_y := subPoints[0][1]
@@ -65,6 +100,8 @@ func areaRect(subPoints [][]int) int {
 	point3_y := subPoints[2][1]
 	point4_x := subPoints[3][0]
 	point4_y := subPoints[3][1]
+	//check if or not three points in one line
+	//1, one point
 	if (point1_x == point2_x && point2_x == point3_x) ||
 		(point1_x == point2_x && point2_x == point4_x) ||
 		(point1_x == point3_x && point3_x == point4_x) ||
@@ -77,6 +114,7 @@ func areaRect(subPoints [][]int) int {
 		(point2_y == point3_y && point3_y == point4_y) {
 		return 0
 	}
+	//2, in one line
 	if isBoomerang(subPoints[0], subPoints[1], subPoints[2]) == false ||
 		isBoomerang(subPoints[0], subPoints[1], subPoints[3]) == false ||
 		isBoomerang(subPoints[1], subPoints[2], subPoints[3]) == false {
@@ -100,6 +138,8 @@ func areaRect(subPoints [][]int) int {
 			disArray = append(disArray, dis(subPoints[i][0], subPoints[i][1], subPoints[j][0], subPoints[j][1]))
 		}
 	}
+	// check if or not a rectangle by uniq point distance should be 3 values left
+	// and the one value plus other value equal to another value
 	sort.Ints(disArray)
 	// fmt.Println(disArray)
 	for i := 0; i < len(disArray); i++ {
