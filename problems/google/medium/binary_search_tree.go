c113a11a758b61fe3145d67145a899553356ec11
hwsun <hwsun@freewheel.tv>
2019-06-11 22:50:13 +0800

    binary search tree with divide conquer

--- problems/google/medium/binary_search_tree.go
+++ problems/google/medium/binary_search_tree.go
@@ -7,25 +7,47 @@ https://leetcode.com/problems/binary-search-tree-iterator
  *     Right *TreeNode
  * }
  */
- type BSTIterator struct {
-    
+type BSTIterator struct {
+	 queue []int
 }
 
 
 func Constructor(root *TreeNode) BSTIterator {
-    
+	queue := inOrder(root)
+	return BSTIterator{queue:queue}
 }
 
+func inOrder(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+	queue := make([]int, 0)
+
+	// divided
+	left := inOrder(root.Left)
+	right := inOrder(root.Right)
+
+	// conquer
+	queue = append(queue, left...)
+	queue = append(queue, root.Val)
+	queue = append(queue, right...)
+	return queue
+}
 
 /** @return the next smallest number */
 func (this *BSTIterator) Next() int {
-    
+	queue := 0
+	if len(this.queue) > 0 {
+		queue = this.queue[0]
+		this.queue = this.queue[1:]
+	}
+	return queue
 }
 
 
 /** @return whether we have a next smallest number */
 func (this *BSTIterator) HasNext() bool {
-    
+	return len(this.queue) > 0
 }
 
 
