714397bbf71ce9fde748373a1172b1f8a68f8e42
hwsun <hwsun@freewheel.tv>
2019-06-13 00:05:06 +0800

    remove node in BST with go

--- problems/common/test.go
+++ problems/common/test.go
@@ -2,87 +2,35 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func main() {
 	// [[0, 30],[5, 10],[15, 20]]
 	// [[9,10],[4,9],[4,17]]
-	res := generateParenthesis(6)
+	res := subarraySum([]int{1, 2, 3}, 3)
+	// res := subarraySum([]int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 0)
 	fmt.Println(res)
 }
 
-func generateParenthesis(n int) []string {
-	if n == 0 {
-		return []string{}
-	}
-	nums := make([]int, 0)
-	visit := make(map[int]bool)
-	uniq := make(map[string]bool)
-	res := make([][]int, 0)
-	entry := make([]int, 0)
-	result := make([]string, 0)
-	for i := 0; i < n; i++ {
-		nums = append(nums, 1)
-	}
-	for i := 0; i < n; i++ {
-		nums = append(nums, -1)
-	}
-	dfs(nums, visit, entry, &res, uniq)
-	for _, item := range res {
-		str := ""
-		for _, i := range item {
-			if i == 1 {
-				str += "("
-			} else if i == -1 {
-				str += ")"
-			}
-		}
-		result = append(result, str)
-	}
-	return result
-}
-
-func dfs(nums []int, visit map[int]bool, entry []int, res *[][]int, uniq map[string]bool) {
-	if len(entry) == len(nums) {
-		cpy := make([]int, len(entry))
-		copy(cpy, entry)
-		str := ""
-		for i := 0; i < len(cpy); i++ {
-			str += strconv.Itoa(cpy[i])
-		}
-		// fmt.Println(str)
-		if v, ok := uniq[str]; ok && v == true {
-			return
-		}
-		*res = append(*res, cpy)
-		uniq[str] = true
-		return
-	}
+func subarraySum(nums []int, k int) int {
+	res := 0
 	for i := 0; i < len(nums); i++ {
-		// if i != 0 && nums[i-1] == nums[i] {
-		// 	continue
-		// }
-		if v, ok := visit[i]; ok && v == true {
-			continue
+		if nums[i] == k {
+			res++
 		}
-		entry = append(entry, nums[i])
-		visit[i] = true
-		if isValid(entry) == true {
-			dfs(nums, visit, entry, res, uniq)
+		if i == len(nums)-1 {
+			// if nums[i] == k {
+			// 	res++
+			// }
+			break
 		}
-		entry = entry[:len(entry)-1]
-		visit[i] = false
-	}
-}
-
-func isValid(arr []int) bool {
-	cur := 0
-	for _, i := range arr {
-		cur += i
-		if cur < 0 {
-			return false
+		target := k - nums[i]
+		for j := i + 1; j < len(nums); j++ {
+			if nums[j] == target {
+				res++
+			}
+			target -= nums[j]
 		}
 	}
-	return true
+	return res
 }
---
+++ problems/medium/delete_node_in_BST.go
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/delete-node-in-a-bst/
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func deleteNode(root *TreeNode, key int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val == key {
+		if root.Left == nil && root.Right == nil {
+			root = nil
+		} else if root.Left != nil && root.Right != nil {
+			max := findMax(root.Left)
+			root.Val = max
+			root.Left = deleteNode(root.Left, max)
+		} else if root.Left == nil {
+			root = root.Right
+		} else if root.Right == nil {
+			root = root.Left
+		}
+	} else if root.Val > key {
+		root.Left = deleteNode(root.Left, key)
+	} else if root.Val < key {
+		root.Right = deleteNode(root.Right, key)
+	}
+	return root
+}
+func findMax(root *TreeNode) int {
+	if root.Right == nil {
+		return root.Val
+	}
+	return findMax(root.Right)
+}
