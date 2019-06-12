8cccf8a630e0dcee1a54ef43b1fdd5982d46a087
hwsun <hwsun@freewheel.tv>
2019-06-12 19:18:15 +0800

    generateParenthesis with DFS but TLE

--- problems/common/test.go
+++ problems/common/test.go
@@ -2,48 +2,87 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"sort"
+	"strconv"
 )
 
 func main() {
 	// [[0, 30],[5, 10],[15, 20]]
 	// [[9,10],[4,9],[4,17]]
-	res := minMeetingRooms([][]int{[]int{9, 10}, []int{4, 9}, []int{4, 17}})
+	res := generateParenthesis(6)
 	fmt.Println(res)
 }
 
-func minMeetingRooms(intervals [][]int) int {
-	if len(intervals) == 0 {
-		return 0
+func generateParenthesis(n int) []string {
+	if n == 0 {
+		return []string{}
 	}
-	m := make(map[int]int, 0)
-	arr := make([]int, 0)
-	res := 0
-	for i := 0; i < len(intervals); i++ {
-		item := intervals[i]
-		if _, ok := m[item[0]]; ok {
-			m[item[0]] += 1
-		} else {
-			m[item[0]] = 1
-		}
-		if _, ok := m[item[1]]; ok {
-			m[item[1]] -= 1
-		} else {
-			m[item[1]] = -1
-		}
-		arr = append(arr, item[0])
-		arr = append(arr, item[1])
+	nums := make([]int, 0)
+	visit := make(map[int]bool)
+	uniq := make(map[string]bool)
+	res := make([][]int, 0)
+	entry := make([]int, 0)
+	result := make([]string, 0)
+	for i := 0; i < n; i++ {
+		nums = append(nums, 1)
 	}
-	cnt := 0
-	// fmt.Println(m)
-	sort.Ints(arr)
-	for k, v := range arr {
-		if k != 0 && v == arr[k-1] {
+	for i := 0; i < n; i++ {
+		nums = append(nums, -1)
+	}
+	dfs(nums, visit, entry, &res, uniq)
+	for _, item := range res {
+		str := ""
+		for _, i := range item {
+			if i == 1 {
+				str += "("
+			} else if i == -1 {
+				str += ")"
+			}
+		}
+		result = append(result, str)
+	}
+	return result
+}
+
+func dfs(nums []int, visit map[int]bool, entry []int, res *[][]int, uniq map[string]bool) {
+	if len(entry) == len(nums) {
+		cpy := make([]int, len(entry))
+		copy(cpy, entry)
+		str := ""
+		for i := 0; i < len(cpy); i++ {
+			str += strconv.Itoa(cpy[i])
+		}
+		// fmt.Println(str)
+		if v, ok := uniq[str]; ok && v == true {
+			return
+		}
+		*res = append(*res, cpy)
+		uniq[str] = true
+		return
+	}
+	for i := 0; i < len(nums); i++ {
+		// if i != 0 && nums[i-1] == nums[i] {
+		// 	continue
+		// }
+		if v, ok := visit[i]; ok && v == true {
 			continue
 		}
-		cnt += m[v]
-		res = int(math.Max(float64(res), float64(cnt)))
+		entry = append(entry, nums[i])
+		visit[i] = true
+		if isValid(entry) == true {
+			dfs(nums, visit, entry, res, uniq)
+		}
+		entry = entry[:len(entry)-1]
+		visit[i] = false
 	}
-	return res
+}
+
+func isValid(arr []int) bool {
+	cur := 0
+	for _, i := range arr {
+		cur += i
+		if cur < 0 {
+			return false
+		}
+	}
+	return true
 }
--- problems/google/medium/generateParenthesis.go
+++ problems/google/medium/generateParenthesis.go
@@ -1,3 +1,5 @@
+import "strconv"
+
 // https://leetcode.com/problems/generate-parentheses/
 // Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
 
@@ -14,6 +16,76 @@
 // "(" means 1 ")" means -1
 // if sum >=0 it's valid
 func generateParenthesis(n int) []string {
+	if n == 0 {
+		return []string{}
+	}
+	nums := make([]int, 0)
+	visit := make(map[int]bool)
+	uniq := make(map[string]bool)
+	res := make([][]int, 0)
+	entry := make([]int, 0)
+	result := make([]string, 0)
+	for i := 0; i < n; i++ {
+		nums = append(nums, 1)
+	}
+	for i := 0; i < n; i++ {
+		nums = append(nums, -1)
+	}
+	dfs(nums, visit, entry, &res, uniq)
+	for _, item := range res {
+		str := ""
+		for _, i := range item {
+			if i == 1 {
+				str += "("
+			} else if i == -1 {
+				str += ")"
+			}
+		}
+		result = append(result, str)
+	}
+	return result
+}
 
-	dfs(nums, visit, &entry, &res)
+func dfs(nums []int, visit map[int]bool, entry []int, res *[][]int, uniq map[string]bool) {
+	if len(entry) == len(nums) {
+		cpy := make([]int, len(entry))
+		copy(cpy, entry)
+		str := ""
+		for i := 0; i < len(cpy); i++ {
+			str += strconv.Itoa(cpy[i])
+		}
+		// fmt.Println(str)
+		if v, ok := uniq[str]; ok && v == true {
+			return
+		}
+		*res = append(*res, cpy)
+		uniq[str] = true
+		return
+	}
+	for i := 0; i < len(nums); i++ {
+		// if i != 0 && nums[i-1] == nums[i] {
+		// 	continue
+		// }
+		if v, ok := visit[i]; ok && v == true {
+			continue
+		}
+		entry = append(entry, nums[i])
+		visit[i] = true
+		if isValid(entry) == true {
+			dfs(nums, visit, entry, res, uniq)
+		}
+		entry = entry[:len(entry)-1]
+		visit[i] = false
+	}
+}
+
+func isValid(arr []int) bool {
+	cur := 0
+	for _, i := range arr {
+		cur += i
+		if cur < 0 {
+			return false
+		}
+	}
+	return true
 }
