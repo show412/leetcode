b2497f90320bbceadbfa13a6319b8dd4df656c70
hwsun <hwsun@freewheel.tv>
2019-06-21 16:51:40 +0800

    first uniq character

--- problems/common/test.go
+++ problems/common/test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func main() {
@@ -10,43 +9,36 @@ func main() {
 	// str := "aaaa3[a2[cc]]2[bc]"
 	// data := reg.FindAllStringSubmatch(str, -1)
 	// fmt.Println(data)
-	res := decodeString("3[z]2[2[y]pq4[2[jk]e1[f]]]ef")
+	res := firstUniqChar("aaaa")
 	fmt.Println(res)
 }
 
-func decodeString(s string) string {
-	sByte := []byte(s)
-	start := 0
-	decodeS := decode(sByte, &start)
-	return string(decodeS)
-}
-
-func decode(s []byte, i *int) []byte {
-	var res []byte
-	var numByte []byte
-
-	for *i < len(s) && s[*i] != ']' {
-		if is_digit(s[*i]) == true {
-			numByte = append(numByte, s[*i])
-			(*i)++
-		} else if s[*i] == '[' {
-			k, _ := strconv.Atoi(string(numByte))
-			(*i)++
-			sub := decode(s, i)
-			for j := 0; j < k; j++ {
-				res = append(res, sub...)
-			}
-			(*i)++
-			// notice we need to clear the numByte because there will add last number
-			numByte = []byte{}
-		} else {
-			res = append(res, s[*i])
-			(*i)++
+func firstUniqChar(s string) int {
+	if s == "" {
+		return -1
+	}
+	m := make(map[byte]int, 0)
+	a := make(map[byte]bool, len(s))
+	for i := 0; i < len(s); i++ {
+		_, ok1 := m[s[i]]
+		_, ok2 := a[s[i]]
+		if ok1 {
+			delete(m, s[i])
+			a[s[i]] = true
+		} else if !ok1 && !ok2 {
+			m[s[i]] = i
+		} else if !ok1 && ok2 {
+			continue
+		}
+	}
+	if len(m) == 0 {
+		return -1
+	}
+	res := len(s)
+	for _, v := range m {
+		if v < res {
+			res = v
 		}
 	}
 	return res
-}
-
-func is_digit(c byte) bool {
-	return c >= '0' && c <= '9'
 }
--- problems/google/easy/first_uniq_character.go
+++ problems/google/easy/first_uniq_character.go
@@ -12,5 +12,63 @@ return 2.
 Note: You may assume the string contain only lowercase letters.
 */
 func firstUniqChar(s string) int {
+	if s == "" {
+		return -1
+	}
+	m := make(map[byte]int, 0)
+	a := make(map[byte]bool, len(s))
+	for i := 0; i < len(s); i++ {
+		_, ok1 := m[s[i]]
+		_, ok2 := a[s[i]]
+		if ok1 {
+			delete(m, s[i])
+			a[s[i]] = true
+		} else if !ok1 && !ok2 {
+			m[s[i]] = i
+		} else if !ok1 && ok2 {
+			continue
+		}
+	}
+	if len(m) == 0 {
+		return -1
+	}
+	res := len(s)
+	for _, v := range m {
+		if v < res {
+			res = v
+		}
+	}
+	return res
+}
 
+// slow and fast pointer
+func firstUniqChar(s string) int {
+	if len(s) == 0 {
+		return -1
+	}
+	lenny := len(s)
+	if lenny == 1 {
+		return 0
+	}
+
+	cc := []byte(s)
+	slow := 0
+	fast := 1
+	count := make([]int, 256)
+	count[cc[slow]]++
+	for fast < lenny {
+		count[cc[fast]]++
+		for slow < lenny && count[cc[slow]] > 1 {
+			slow++
+		}
+		if slow >= lenny {
+			return -1
+		}
+		if count[cc[slow]] == 0 {
+			count[cc[slow]]++
+			fast = slow
+		}
+		fast++
+	}
+	return slow
 }
