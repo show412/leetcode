f30432b4f2fe8f4d95f4dd117626fc4c25ced8ee
hwsun <hwsun@freewheel.tv>
2019-06-21 22:35:53 +0800

    pow use binary sum is n to caculate pow

--- problems/common/test.go
+++ problems/common/test.go
@@ -9,36 +9,42 @@ func main() {
 	// str := "aaaa3[a2[cc]]2[bc]"
 	// data := reg.FindAllStringSubmatch(str, -1)
 	// fmt.Println(data)
-	res := firstUniqChar("aaaa")
+	res := myPow(2.00000, 10)
 	fmt.Println(res)
 }
 
-func firstUniqChar(s string) int {
-	if s == "" {
-		return -1
+func myPow(x float64, n int) float64 {
+	if n == 0 {
+		return 1.00000
 	}
-	m := make(map[byte]int, 0)
-	a := make(map[byte]bool, len(s))
-	for i := 0; i < len(s); i++ {
-		_, ok1 := m[s[i]]
-		_, ok2 := a[s[i]]
-		if ok1 {
-			delete(m, s[i])
-			a[s[i]] = true
-		} else if !ok1 && !ok2 {
-			m[s[i]] = i
-		} else if !ok1 && ok2 {
-			continue
+	if n == 1 {
+		return x
+	}
+	if x == 1.00000 {
+		return x
+	}
+	if x == -1.00000 {
+		if n%2 == 0 {
+			return 1.00000
+		} else {
+			return -1.00000
 		}
 	}
-	if len(m) == 0 {
-		return -1
+
+	if n < 0 {
+		x = 1 / x
+		n = -n
 	}
-	res := len(s)
-	for _, v := range m {
-		if v < res {
-			res = v
+
+	res := 1.00000
+	for n != 0 {
+		// it should be on the before x *= x
+		if n&1 == 1 {
+			res *= x
 		}
+		// get x value on the next loop
+		x *= x
+		n = (n >> 1)
 	}
 	return res
 }
--- problems/google/medium/pow.go
+++ problems/google/medium/pow.go
@@ -20,6 +20,70 @@ Note:
 -100.0 < x < 100.0
 n is a 32-bit signed integer, within the range [−231, 231 − 1]
 */
+// TC is O(n) and SC is O(1)
 func myPow(x float64, n int) float64 {
+	if n == 0 {
+		return 1.00000
+	}
+	if x == 1.00000 {
+		return x
+	}
+	if x == -1.00000 {
+		if n%2 == 0 {
+			return 1.00000
+		} else {
+			return -1.00000
+		}
+	}
 
+	res := 1.00
+	if n < 0 {
+		x = 1 / x
+		n = -n
+	}
+	for n > 0 {
+		res *= x
+		n--
+	}
+	return float64(res)
+}
+
+// last soultion is brute force to solve this question
+// there are one better solution with better TC and SC
+// TC is O(logn) and SC is O(1)
+// refer to https://blog.csdn.net/Lison_Zhu/article/details/80031375
+// because the x^n = x ^ (∑2^i) = 某些2进制相加总会等于n
+// 这样就可以把n变为2进制 看哪位是1 根据2进制计算就会减少时间复杂度
+func myPow(x float64, n int) float64 {
+	if n == 0 {
+		return 1.00000
+	}
+	if n == 1 {
+		return x
+	}
+	if x == 1.00000 {
+		return x
+	}
+	if x == -1.00000 {
+		if n%2 == 0 {
+			return 1.00000
+		} else {
+			return -1.00000
+		}
+	}
+
+	if n < 0 {
+		x = 1 / x
+		n = -n
+	}
+
+	res := 1.00000
+	for n != 0 {
+		x *= x
+		if n&1 == 1 {
+			res *= x
+		}
+		n = n >> 1
+	}
+	return res
 }
