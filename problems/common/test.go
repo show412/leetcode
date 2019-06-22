20a475ef6bafeca5e33b46a57f0ab5bbe93f512d
hwsun <hwsun@freewheel.tv>
2019-06-22 21:24:45 +0800

    common solution for 1 ≤ a[i] ≤ n (n = size of array)

--- problems/common/test.go
+++ problems/common/test.go
@@ -9,49 +9,26 @@ func main() {
 	// str := "aaaa3[a2[cc]]2[bc]"
 	// data := reg.FindAllStringSubmatch(str, -1)
 	// fmt.Println(data)
-	res := addStrings("123", "1789")
+	res := findDisappearedNumbers([]int{4, 3, 2, 7, 8, 2, 3, 1})
 	fmt.Println(res)
 }
 
 // Input:  [0,1,2,4,5,7]
 // Output: ["0->2","4->5","7"]
-func addStrings(num1 string, num2 string) string {
-	// the result length should be the longest between num1 and num2 to add 1
-	// because it maybe have a carry
-	res := make([]byte, len(num1)+1)
-	if len(num1) < len(num2) {
-		res = make([]byte, len(num2)+1)
-	}
-	// from end of num1 and num2 to add by byte
-	// if one of num1 or num2 has no digits, add the left disgits into res
-	j, z := len(num1)-1, len(num2)-1
-	for i := len(res) - 1; i >= 0; i-- {
-		if j >= 0 && z < 0 {
-			res[i] = num1[j] - '0'
-		} else if j < 0 && z >= 0 {
-			res[i] = num2[z] - '0'
-		} else if j >= 0 && z >= 0 {
-			res[i] = (num1[j] - '0') + (num2[z] - '0')
-		}
-		j--
-		z--
-	}
-
-	// Deal with the carry
-	for i := len(res) - 1; i > 0; i-- {
-		res[i-1] = res[i]/10 + res[i-1]
-		res[i] = res[i] % 10
-	}
-	// work out the result
-	var str []byte
-	for i := 0; i < len(res); i++ {
-		if i == 0 && res[i] == 0 {
+func findDisappearedNumbers(nums []int) []int {
+	nlen := len(nums)
+	for i := 0; i < nlen; {
+		if nums[i] != i+1 && nums[nums[i]-1] != nums[i] {
+			nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i]
 			continue
 		}
-		str = append(str, res[i]+'0')
+		i++
 	}
-	if len(str) == 0 {
-		return "0"
+	var res []int
+	for i := 0; i < nlen; i++ {
+		if nums[i] != i+1 {
+			res = append(res, i+1)
+		}
 	}
-	return string(str)
+	return res
 }
