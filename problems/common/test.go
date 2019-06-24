886cd03526af0728225711af0f4459f195f2e2cd
hwsun <hwsun@freewheel.tv>
2019-06-24 23:49:42 +0800

    lengthLongestPath

--- problems/common/test.go
+++ problems/common/test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
@@ -9,40 +10,53 @@ func main() {
 	// str := "aaaa3[a2[cc]]2[bc]"
 	// data := reg.FindAllStringSubmatch(str, -1)
 	// fmt.Println(data)
-	res := firstUniqChar("leetcode")
+	res := lengthLongestPath("dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext")
 	// [1, 1, 4, 2, 1, 1, 0, 0]
 	fmt.Println(res)
 }
 
-func firstUniqChar(s string) int {
-	if len(s) == 0 {
-		return -1
-	}
-	lenny := len(s)
-	if lenny == 1 {
+func lengthLongestPath(input string) int {
+	if input == "" {
 		return 0
 	}
+	inputArray := strings.Split(input, "\n")
+	res := len(inputArray[0])
+	// ["dir" "\tsubdir1" "\tsubdir2" "\t\tfile.ext"]
 
-	cc := []byte(s)
-	slow := 0
-	fast := 1
-	count := make([]int, 256)
-	count[cc[slow]]++
-	for fast < lenny {
-		count[cc[fast]]++
-		for slow < lenny && count[cc[slow]] > 1 {
-			slow++
+	for start := 1; start < len(inputArray); start++ {
+		cur := inputArray[start]
+		length := len(cur)
+		end := start + 1
+		if end < len(inputArray) && cur[0] == '\t' && cur[1] != '\t' {
+			fmt.Println(start)
+			if strings.IndexByte(cur, '.') >= 0 {
+				res = max(res, res+len(cur))
+				continue
+			}
+			next := inputArray[end]
+
+			for end < len(inputArray) && (end-start) < len(next) && next[end-start] == '\t' {
+				length += len(next)
+				if strings.IndexByte(next, '.') >= 0 {
+					res = max(res, res+length-end+start)
+					break
+				}
+				end++
+			}
+		} else {
+			start = end + 1
+			continue
 		}
-		if slow >= lenny {
-			return -1
-		}
-		if count[cc[slow]] == 0 {
-			count[cc[slow]]++
-			fast = slow
-		}
-		fast++
-		fmt.Println(slow)
 	}
+	if res == len(inputArray[0]) {
+		return 0
+	}
+	return res
+}
 
-	return slow
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
