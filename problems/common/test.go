4d6e51d135eb1940ec9160edf0d20f1f3bcee714
hwsun <hwsun@freewheel.tv>
2019-06-27 16:29:43 +0800

    max profit with DP

--- problems/common/test.go
+++ problems/common/test.go
@@ -9,26 +9,35 @@ func main() {
 	// str := "aaaa3[a2[cc]]2[bc]"
 	// data := reg.FindAllStringSubmatch(str, -1)
 	// fmt.Println(data)
-	res := maxProfit([]int{1, 2, 4})
+	// 1, 2, 3, 0, 2
+	res := maxProfit([]int{1, 2, 3, 0, 2})
 	fmt.Println(res)
 }
 
+// 这个算法的问题是 sell 不一定就要在 比buy的价格大的那天 sell, 是一个动态的过程
+// so it looks like a DP solution
+/*
+	buy[i]  = max(rest[i-1]-price, buy[i-1])
+ 	sell[i] = max(buy[i-1]+price, sell[i-1])
+	rest[i] = max(sell[i-1], buy[i-1], rest[i-1])
+*/
 func maxProfit(prices []int) int {
 	if len(prices) <= 1 {
 		return 0
 	}
-	res := 0
-	// var benift []int
-	for i := 0; i < len(prices)-1; i++ {
-		for j := i + 1; j < len(prices); j++ {
-			if prices[j] > prices[i] {
-				res = max(res, res+prices[j]-prices[i])
-				j++
-				i = j + 1
-			}
-		}
+	buy := make([]int, len(prices))
+	sell := make([]int, len(prices))
+	cooldown := make([]int, len(prices))
+	buy[0] = -prices[0]
+	sell[0] = 0
+	cooldown[0] = 0
+	for i := 1; i < len(prices); i++ {
+		price := prices[i]
+		buy[i] = max(cooldown[i-1]-price, buy[i-1])
+		sell[i] = max(buy[i-1]+price, sell[i-1])
+		cooldown[i] = maxThree(sell[i-1], buy[i-1], cooldown[i-1])
 	}
-	return res
+	return sell[len(prices)-1]
 }
 
 func max(a int, b int) int {
@@ -36,4 +45,21 @@ func max(a int, b int) int {
 		return a
 	}
 	return b
+}
+
+func maxThree(a int, b int, c int) int {
+	if a > b {
+		if a >= c {
+			return a
+		} else {
+			return c
+		}
+	} else {
+		if b >= c {
+			return b
+		} else {
+			return c
+		}
+	}
+	return a
 }
