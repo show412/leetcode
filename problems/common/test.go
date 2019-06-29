7a540c2154376b3baab029dbf175aac01d229725
hwsun <hwsun@freewheel.tv>
2019-06-29 14:29:49 +0800

    judgeCircle with simple solution

--- problems/common/test.go
+++ problems/common/test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"sort"
 	// "math"
 )
 
@@ -12,40 +11,25 @@ func main() {
 	// data := reg.FindAllStringSubmatch(str, -1)
 	// fmt.Println(data)
 	// 1, 2, 3, 0, 2
-	res := reconstructQueue([][]int{[]int{7, 0}, []int{4, 4}, []int{7, 1}, []int{5, 0}, []int{6, 1}, []int{5, 2}})
+	res := judgeCircle("UD")
 	fmt.Println(res)
 }
 
-func reconstructQueue(people [][]int) [][]int {
-	if len(people) == 0 {
-		return people
-	}
-	res := make([][]int, len(people))
-	sort.Sort(peopleSlice(people))
-	// fmt.Println(people)
-	for i := 0; i < len(people); i++ {
-		start := 0
-		for j := 0; j < len(res); j++ {
-			if res[j] != nil {
-				continue
-			}
-			if start == people[i][1] {
-				res[j] = people[i]
-			}
-			start++
+func judgeCircle(moves string) bool {
+	x, y := 0, 0
+	for i := 0; i < len(moves); i++ {
+		if moves[i] == 'U' {
+			y++
+		}
+		if moves[i] == 'D' {
+			y--
+		}
+		if moves[i] == 'R' {
+			x++
+		}
+		if moves[i] == 'L' {
+			x--
 		}
 	}
-	return res
-}
-
-type peopleSlice [][]int
-
-func (a peopleSlice) Len() int {
-	return len(a)
-}
-func (a peopleSlice) Less(i, j int) bool {
-	return a[i][0] < a[j][0] || (a[i][0] == a[j][0] && a[i][1] > a[j][1])
-}
-func (a peopleSlice) Swap(i, j int) {
-	a[i], a[j] = a[j], a[i]
+	return x == 0 && y == 0
 }
