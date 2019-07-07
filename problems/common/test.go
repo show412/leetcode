505988845f4708d04c0d6f913d472493ca9fa032
hwsun <hwsun@freewheel.tv>
2019-07-07 19:49:51 +0800

    expressive Words

--- problems/common/test.go
+++ problems/common/test.go
@@ -6,88 +6,90 @@ import (
 )
 
 func main() {
-	// "e##e#o##oyof##q"
-	// "e##e#fq##o##oyof##q"
-	// "xywrrmp", "xywrrmu#p"
-	// "bxj##tw"
-	// "bxj###tw"
-	// 	"bxj##tw"
-	// "bxo#j##tw"
-	res := calcEquation([][]string{[]string{"a", "b"}, []string{"b", "c"}}, []float64{2.0, 3.0}, [][]string{[]string{"a", "c"}, []string{"b", "a"}, []string{"a", "e"}, []string{"a", "a"}, []string{"x", "x"}})
+	/*
+		"heeellooo"
+		["axxxrrzzz"]
+	*/
+	/*
+		"zzzzzyyyyy"
+		["zzyy","zy","zyy"]
+	*/
+	/*
+		"heeellooo"
+		"hello", "hi", "helo"
+	*/
+	/*
+			"dddiiiinnssssssoooo"
+		  ["dinnssoo","ddinso","ddiinnso","ddiinnssoo","ddiinso","dinsoo","ddiinsso","dinssoo","dinso"]
+	*/
+	res := expressiveWords("heeellooo", []string{"axxxrrzzz"})
 	fmt.Println(res)
 }
 
-func calcEquation(equations [][]string, values []float64, queries [][]string) []float64 {
-	m := make(map[string]string, 0)
-	res := make([]float64, len(queries))
-	dist := make(map[string]float64, 0)
-
-	for i := 0; i < len(equations); i++ {
-		unity(m, dist, values[i], equations[i][0], equations[i][1])
-		// fmt.Println(m)
+func expressiveWords(S string, words []string) int {
+	if len(words) == 0 {
+		return 0
 	}
-	// fmt.Println(m)
-	// fmt.Println(dist)
-	for i := 0; i < len(queries); i++ {
-		_, ok1 := m[queries[i][0]]
-		_, ok2 := m[queries[i][1]]
-		if !ok1 || !ok2 {
-			res[i] = -1.0
-			continue
-		}
-		// fmt.Println("***")
-		// fmt.Println(v1)
-		// fmt.Println(v2)
-		if find(m, dist, queries[i][0]) == find(m, dist, queries[i][1]) {
-			// fmt.Println("*****")
-			// fmt.Println(m)
-			// fmt.Println(m)
-			// fmt.Println(dist)
-			res[i] = float64(dist[queries[i][0]] / dist[queries[i][1]])
+	res := 0
+	SByteArray, SByteNum := generatePair(S)
+	for i := 0; i < len(words); i++ {
+		word := words[i]
+		wordByteArray, wordByteNum := generatePair(word)
+		// fmt.Println("****")
+		// fmt.Println(SByteArray)
+		// fmt.Println(wordByteArray)
+		// fmt.Println(SByteNum)
+		// fmt.Println(wordByteNum)
+		if sliceEqual(SByteArray, wordByteArray) == true {
+			for j := 0; j < len(wordByteNum); j++ {
+				if SByteNum[j] < 3 && SByteNum[j] != wordByteNum[j] || SByteNum[j] < wordByteNum[j] {
+					break
+				}
+				// fmt.Println(j)
+				if j == len(wordByteNum)-1 {
+					res++
+				}
+
+			}
 		}
 	}
 	return res
 }
 
-func add(m map[string]string, dist map[string]float64, s string) {
-	_, ok := m[s]
-
-	if !ok {
-		m[s] = s
-		dist[s] = 1.0
+func generatePair(s string) ([]byte, []int) {
+	var byteArray []byte
+	var byteNum []int
+	byteArray = append(byteArray, s[0])
+	num := 1
+	for i := 1; i < len(s); i++ {
+		if s[i] == s[i-1] {
+			num++
+		} else {
+			byteArray = append(byteArray, s[i])
+			byteNum = append(byteNum, num)
+			num = 1
+		}
+		if i == len(s)-1 {
+			byteNum = append(byteNum, num)
+		}
 	}
-
-	return
+	return byteArray, byteNum
 }
 
-func unity(m map[string]string, dist map[string]float64, value float64, s1 string, s2 string) {
-	add(m, dist, s1)
-	add(m, dist, s2)
-	p1 := find(m, dist, s1)
-	p2 := find(m, dist, s2)
-	m[p1] = p2
-	dist[p1] = float64((dist[s2] * value) / dist[s1])
-	return
-}
-
-func find(m map[string]string, dist map[string]float64, s string) string {
-	v, ok := m[s]
-
-	if !ok {
-		m[s] = s
+func sliceEqual(s1, s2 []byte) bool {
+	if len(s1) != len(s2) {
+		return false
 	}
 
-	if s != v {
-
-		parent := find(m, dist, v)
-		// fmt.Println("*****")
-		// fmt.Println(s)
-		// fmt.Println(parent)
-		m[s] = parent
-		dist[s] = dist[s] * dist[v]
-		// fmt.Println(m)
-		// fmt.Println(dist)
+	if (s1 == nil) != (s2 == nil) {
+		return false
 	}
 
-	return m[s]
+	for i, v := range s1 {
+		if v != s2[i] {
+			return false
+		}
+	}
+
+	return true
 }
