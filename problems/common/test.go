242464b98861f1c7827d2fa470784d585a28093c
hwsun <hwsun@freewheel.tv>
2019-07-09 00:07:18 +0800

    CampusBikes solution

--- problems/common/test.go
+++ problems/common/test.go
@@ -2,94 +2,81 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"sort"
 	// "math"
 )
 
 func main() {
 	/*
-		"heeellooo"
-		["axxxrrzzz"]
+			[[240,446],[745,948],[345,136],[341,68],[990,165],[165,580],[133,454],[113,527]]
+		[[696,140],[95,393],[935,185],[767,205],[387,767],[214,960],[804,710],[956,307]]
 	*/
-	/*
-		"zzzzzyyyyy"
-		["zzyy","zy","zyy"]
-	*/
-	/*
-		"heeellooo"
-		"hello", "hi", "helo"
-	*/
-	/*
-			"dddiiiinnssssssoooo"
-		  ["dinnssoo","ddinso","ddiinnso","ddiinnssoo","ddiinso","dinsoo","ddiinsso","dinssoo","dinso"]
-	*/
-	res := expressiveWords("heeellooo", []string{"axxxrrzzz"})
+
+	res := assignBikes([][]int{[]int{0, 0}, []int{1, 1}, []int{2, 0}}, [][]int{[]int{1, 0}, []int{2, 2}, []int{2, 1}})
 	fmt.Println(res)
 }
 
-func expressiveWords(S string, words []string) int {
-	if len(words) == 0 {
-		return 0
-	}
-	res := 0
-	SByteArray, SByteNum := generatePair(S)
-	for i := 0; i < len(words); i++ {
-		word := words[i]
-		wordByteArray, wordByteNum := generatePair(word)
-		// fmt.Println("****")
-		// fmt.Println(SByteArray)
-		// fmt.Println(wordByteArray)
-		// fmt.Println(SByteNum)
-		// fmt.Println(wordByteNum)
-		if sliceEqual(SByteArray, wordByteArray) == true {
-			for j := 0; j < len(wordByteNum); j++ {
-				if SByteNum[j] < 3 && SByteNum[j] != wordByteNum[j] || SByteNum[j] < wordByteNum[j] {
-					break
-				}
-				// fmt.Println(j)
-				if j == len(wordByteNum)-1 {
-					res++
-				}
+type DistanceSlice []Distance
 
-			}
+func (d DistanceSlice) Len() int {
+	return len(d)
+}
+
+func (d DistanceSlice) Less(i, j int) bool {
+	mdi := int(math.Abs(float64(d[i].workerCoordinate[0]-d[i].bikeCoordinate[0]))) +
+		int(math.Abs(float64(d[i].workerCoordinate[1]-d[i].bikeCoordinate[1])))
+	mdj := int(math.Abs(float64(d[j].workerCoordinate[0]-d[j].bikeCoordinate[0]))) +
+		int(math.Abs(float64(d[j].workerCoordinate[1]-d[j].bikeCoordinate[1])))
+	if mdi == mdj && d[i].workerIndex == d[j].workerIndex {
+		return d[i].bikeIndex < d[j].bikeIndex
+	} else if mdi == mdj {
+		return d[i].workerIndex < d[j].workerIndex
+	} else {
+		return mdi < mdj
+	}
+}
+
+func (d DistanceSlice) Swap(i, j int) {
+	d[i], d[j] = d[j], d[i]
+}
+
+type Distance struct {
+	workerIndex      int
+	bikeIndex        int
+	workerCoordinate []int
+	bikeCoordinate   []int
+}
+
+func assignBikes(workers [][]int, bikes [][]int) []int {
+	res := make([]int, len(workers))
+	distanceArray := make([]Distance, 0)
+	workerMap := make(map[int]bool, len(workers))
+	bikeMap := make(map[int]bool, len(bikes))
+	for i := 0; i < len(workers); i++ {
+		for j := 0; j < len(bikes); j++ {
+			item := Distance{
+				workerIndex:      i,
+				bikeIndex:        j,
+				workerCoordinate: workers[i],
+				bikeCoordinate:   bikes[j]}
+			distanceArray = append(distanceArray, item)
 		}
 	}
+	sort.Sort(DistanceSlice(distanceArray))
+	// fmt.Println(distanceArray)
+	// fmt.Println(workerMap)
+
+	for i := 0; i < len(distanceArray); i++ {
+		cur := distanceArray[i]
+		v1, ok1 := workerMap[cur.workerIndex]
+		v2, ok2 := bikeMap[cur.bikeIndex]
+		if !ok1 && !ok2 && v1 != true && v2 != true {
+			res[cur.workerIndex] = cur.bikeIndex
+			workerMap[cur.workerIndex] = true
+			bikeMap[cur.bikeIndex] = true
+		}
+	}
+
 	return res
-}
-
-func generatePair(s string) ([]byte, []int) {
-	var byteArray []byte
-	var byteNum []int
-	byteArray = append(byteArray, s[0])
-	num := 1
-	for i := 1; i < len(s); i++ {
-		if s[i] == s[i-1] {
-			num++
-		} else {
-			byteArray = append(byteArray, s[i])
-			byteNum = append(byteNum, num)
-			num = 1
-		}
-		if i == len(s)-1 {
-			byteNum = append(byteNum, num)
-		}
-	}
-	return byteArray, byteNum
-}
-
-func sliceEqual(s1, s2 []byte) bool {
-	if len(s1) != len(s2) {
-		return false
-	}
-
-	if (s1 == nil) != (s2 == nil) {
-		return false
-	}
-
-	for i, v := range s1 {
-		if v != s2[i] {
-			return false
-		}
-	}
-
-	return true
 }
--- problems/google/medium/CampusBikes.go
+++ problems/google/medium/CampusBikes.go
@@ -1,3 +1,8 @@
+import (
+	"math"
+	"sort"
+)
+
 // https://leetcode.com/problems/campus-bikes/
 /*
 On a campus represented as a 2D grid, there are N workers and M bikes, with N <= M. Each worker and bike is a 2D coordinate on this grid.
@@ -40,20 +45,72 @@ Can you do this in less than O(nlogn) time, where n is the total number of pairs
 
 */
 
-type Bikes [][]int
+/*
+use case:
+		[[240,446],[745,948],[345,136],[341,68],[990,165],[165,580],[133,454],[113,527]]
+		[[696,140],[95,393],[935,185],[767,205],[387,767],[214,960],[804,710],[956,307]]
+*/
 
-func (b Bikes) Len() int {
-	return len(b)
+type DistanceSlice []Distance
+
+func (d DistanceSlice) Len() int {
+	return len(d)
 }
 
-func (b Bikes) Less(i, j int) bool {
-
+func (d DistanceSlice) Less(i, j int) bool {
+	mdi := int(math.Abs(float64(d[i].workerCoordinate[0]-d[i].bikeCoordinate[0]))) +
+		int(math.Abs(float64(d[i].workerCoordinate[1]-d[i].bikeCoordinate[1])))
+	mdj := int(math.Abs(float64(d[j].workerCoordinate[0]-d[j].bikeCoordinate[0]))) +
+		int(math.Abs(float64(d[j].workerCoordinate[1]-d[j].bikeCoordinate[1])))
+	if mdi == mdj && d[i].workerIndex == d[j].workerIndex {
+		return d[i].bikeIndex < d[j].bikeIndex
+	} else if mdi == mdj {
+		return d[i].workerIndex < d[j].workerIndex
+	} else {
+		return mdi < mdj
+	}
 }
 
-func (b Bikes) Swap(i, j int) {
+func (d DistanceSlice) Swap(i, j int) {
+	d[i], d[j] = d[j], d[i]
+}
 
+type Distance struct {
+	workerIndex      int
+	bikeIndex        int
+	workerCoordinate []int
+	bikeCoordinate   []int
 }
 
 func assignBikes(workers [][]int, bikes [][]int) []int {
+	res := make([]int, len(workers))
+	distanceArray := make([]Distance, 0)
+	workerMap := make(map[int]bool, len(workers))
+	bikeMap := make(map[int]bool, len(bikes))
+	for i := 0; i < len(workers); i++ {
+		for j := 0; j < len(bikes); j++ {
+			item := Distance{
+				workerIndex:      i,
+				bikeIndex:        j,
+				workerCoordinate: workers[i],
+				bikeCoordinate:   bikes[j]}
+			distanceArray = append(distanceArray, item)
+		}
+	}
+	sort.Sort(DistanceSlice(distanceArray))
+	// fmt.Println(distanceArray)
+	// fmt.Println(workerMap)
 
+	for i := 0; i < len(distanceArray); i++ {
+		cur := distanceArray[i]
+		v1, ok1 := workerMap[cur.workerIndex]
+		v2, ok2 := bikeMap[cur.bikeIndex]
+		if !ok1 && !ok2 && v1 != true && v2 != true {
+			res[cur.workerIndex] = cur.bikeIndex
+			workerMap[cur.workerIndex] = true
+			bikeMap[cur.bikeIndex] = true
+		}
+	}
+
+	return res
 }
