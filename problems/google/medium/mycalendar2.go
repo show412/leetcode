5a1fdc0c06be068ba3cb2e002a3a58e5a446419a
hwsun <hwsun@freewheel.tv>
2019-07-10 00:03:44 +0800

    mycalendar2 solution

--- problems/google/medium/mycalendar2.go
+++ problems/google/medium/mycalendar2.go
@@ -34,13 +34,43 @@ In calls to MyCalendar.book(start, end), start and end are integers in the range
 */
 
 type MyCalendarTwo struct {
-	var calendar map[int]int
+	// it should be defined into [][]int, because we need to know one range
+	// if it's defined into []int, it's only one point by one point
+	calendarOne [][2]int
+	calendarTwo [][2]int
 }
 
 func Constructor() MyCalendarTwo {
-	return MyCalendarTwo{calendar: make(map[int]int, 0)}
+	return MyCalendarTwo{calendarOne: make([][2]int, 0), calendarTwo: make([][2]int, 0)}
 }
 
 func (this *MyCalendarTwo) Book(start int, end int) bool {
+	for _, v := range this.calendarTwo {
+		// it should have the overlap not the cover
+		if end > v[0] && start < v[1] {
+			return false
+		}
+	}
+	for _, v := range this.calendarOne {
+		// it should have the overlap not the cover
+		if end > v[0] && start < v[1] {
+			this.calendarTwo = append(this.calendarTwo, [2]int{max(start, v[0]), min(end, v[1])})
+		}
+	}
+	this.calendarOne = append(this.calendarOne, [2]int{start, end})
+	return true
+}
 
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a int, b int) int {
+	if a > b {
+		return b
+	}
+	return a
 }
