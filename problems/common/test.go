535208247c4092450ef193262decce5b279260f3
hwsun <hwsun@freewheel.tv>
2019-07-11 23:41:33 +0800

    addBoldTag no to resolve

--- problems/common/test.go
+++ problems/common/test.go
@@ -2,56 +2,138 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	// "math"
 )
 
 func main() {
 	/*
-		"968018661806000118986811000908199810896"
-		"96"
-		"88"
-		"101"
+								Input:
+						s = "aaabbcc"
+						dict = ["aaa","aab","bc"]
+						Output:
+						"<b>aaabbc</b>c"
+
+								Input:
+						s = "abcxyz123"
+						dict = ["abc","123"]
+						Output:
+						"<b>abc</b>xyz<b>123</b>"
+
+						"aaabbcc"
+				["d"]
+
+				"zxioozhirtosxhlkaeociwdsgyqnvxuxizhvfrkestehiiwpmayhlyybhnpgfcxfjltnhlwppzfhxyozlaorrcfdhpbjxwalxtxutnperhobhatxwnvowtolsbsxfwzpyttpksrhhkvtfycpuvztjeeabraqqwustqubzzmjkuzafwcuixdbiuwqgexmlqzlyjxdwjzwjuxopecpcbojkrrceysozvkabzirctyroynqbyomecatnjupiqjexevltvtspnkpkzajzqcagsqnkagrludmpsgczbtkvbbcbjenacgrtowsjxazboxcjdhvjnhcaqsjokztkskntlwpnrecnrfuhuseuhshqoddhutcocgrbvnxgnliiinbjcoydtdlyvfjlmpzegscxcfoacxhwaqusneoptvmfrbljkpsbixsjlbvatkyoillejllsvlqfuvxqaareawgfwwultacnjgepmzqhykqalbqfhxohzfntgiatttqupukfhcvghqjzrutxlwidbfqrssrrnbtbjnsggdvrxrbotvwpofpxrwgcecbcqczuuxlrfpnskznmjdcbqktilxijkilpjwywplxdnirjhgoisfwamuauljoqzbmxlvtzdqocbvrusvxyvslufvbcigggddwlubnjjlxmmmcrcefssuqwvtvmwkhovflsxxraneluwdraknumssfluujdlayviukvaqgumpdefzivsqpjkymhlkltojsbyzmvyusmnuxytexhjcszblvimywfoqwsihtchhqnotxvibjzqhthhojreuuknhbfhzmivqnbofrwktfwlcbajoygobbesqeeiemullkrqwplsmsuvskuifbojrdvtffpqm"
+		["zx","oo","hi","to","xh","ka","oc","wd","gy","nv","ux","zh","fr","es","eh","iw","ma","hl","yb","np","fc","fj","tn","lw","pz","hx","oz","ao","rc","dh","bj"]
+
 	*/
 
-	res := isStrobogrammatic("101")
+	res := addBoldTag("zxioozhirtosxhlkaeociwdsgyqnvxuxizhvfrkestehiiwpmayhlyybhnpgfcxfjltnhlwppzfhxyozlaorrcfdhpbjxwalxtxutnperhobhatxwnvowtolsbsxfwzpyttpksrhhkvtfycpuvztjeeabraqqwustqubzzmjkuzafwcuixdbiuwqgexmlqzlyjxdwjzwjuxopecpcbojkrrceysozvkabzirctyroynqbyomecatnjupiqjexevltvtspnkpkzajzqcagsqnkagrludmpsgczbtkvbbcbjenacgrtowsjxazboxcjdhvjnhcaqsjokztkskntlwpnrecnrfuhuseuhshqoddhutcocgrbvnxgnliiinbjcoydtdlyvfjlmpzegscxcfoacxhwaqusneoptvmfrbljkpsbixsjlbvatkyoillejllsvlqfuvxqaareawgfwwultacnjgepmzqhykqalbqfhxohzfntgiatttqupukfhcvghqjzrutxlwidbfqrssrrnbtbjnsggdvrxrbotvwpofpxrwgcecbcqczuuxlrfpnskznmjdcbqktilxijkilpjwywplxdnirjhgoisfwamuauljoqzbmxlvtzdqocbvrusvxyvslufvbcigggddwlubnjjlxmmmcrcefssuqwvtvmwkhovflsxxraneluwdraknumssfluujdlayviukvaqgumpdefzivsqpjkymhlkltojsbyzmvyusmnuxytexhjcszblvimywfoqwsihtchhqnotxvibjzqhthhojreuuknhbfhzmivqnbofrwktfwlcbajoygobbesqeeiemullkrqwplsmsuvskuifbojrdvtffpqm", []string{"zx", "oo", "hi", "to", "xh", "ka", "oc", "wd", "gy", "nv", "ux", "zh", "fr", "es", "eh", "iw", "ma", "hl", "yb", "np", "fc", "fj", "tn", "lw", "pz", "hx", "oz", "ao", "rc", "dh", "bj"})
 	fmt.Println(res)
 }
 
-func isStrobogrammatic(num string) bool {
-	// oriNum := num
-	if num == "0" {
-		return true
+func addBoldTag(s string, dict []string) string {
+	if len(dict) == 0 {
+		return s
 	}
-	rotated := make([]byte, 0)
-	for i := len(num) - 1; i >= 0; i-- {
-		digit := num[i]
-		if digit == '6' {
-			rotated = append(rotated, '9')
-			continue
+	res := make([]string, 5000)
+	closeSlice := make([][2]int, 0)
+	tagS := ""
+	// boldStart := make(map[int]bool)
+	// boldEnd := make(map[int]bool)
+	// generate close position according to dict
+	for i := 0; i < len(dict); i++ {
+		if len(closeSlice) == 0 && strings.Index(s, dict[i]) >= 0 {
+			first := [2]int{strings.Index(s, dict[i]), strings.Index(s, dict[i]) + len(dict[i]) + 1}
+			closeSlice = append(closeSlice, first)
 		}
-		if digit == '9' {
-			rotated = append(rotated, '6')
-			continue
+		for j := 0; j < len(closeSlice); j++ {
+			curStart := closeSlice[j][0]
+			curEnd := closeSlice[j][1]
+			if strings.Index(s, dict[i]) >= 0 {
+				start := strings.Index(s, dict[i])
+				end := strings.Index(s, dict[i]) + len(dict[i]) + 1
+				//check [curStart, curEnd] overlap or sequence to [start, end]
+				if (start <= curEnd && end >= curStart) || start == curEnd+1 || end == curStart+1 {
+					closeSlice[j] = [2]int{min(start, curStart), max(end, curEnd)}
+				} else {
+					closeSlice = append(closeSlice, [2]int{start, end})
+				}
+			}
+
 		}
-		if digit == '8' {
-			rotated = append(rotated, '8')
-			continue
-		}
-		if digit == '1' {
-			rotated = append(rotated, '1')
-			continue
-		}
-		if digit == '0' {
-			rotated = append(rotated, '0')
-			continue
-		}
-		return false
+	}
+	fmt.Println(closeSlice)
+	for i := 0; i < len(closeSlice); i++ {
+		start := closeSlice[i][0]
+		end := closeSlice[i][1]
+		res[start+2*i] = "<b>"
+		res[end+2*i] = "</b>"
 	}
 
-	// fmt.Println(string(rotated))
-	// fmt.Println(num)
-	if string(rotated) == num {
-		return true
+	for i := 0; i < len(s); i++ {
+		k := i
+		for res[k] != "" {
+			k++
+		}
+		res[k] = string(s[i])
 	}
-	return false
+
+	for i := 0; i < len(res); i++ {
+		if res[i] == "" {
+			break
+		}
+		tagS += res[i]
+	}
+	return tagS
 }
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+/*
+class Solution {
+    public String boldWords(String S, String[] words) {
+        int N = S.length();
+        boolean[] mask = new boolean[N];
+        for (int i = 0; i < N; ++i)
+            for (String word: words) search: {
+                for (int k = 0; k < word.length(); ++k)
+                    if (k+i >= S.length() || S.charAt(k+i) != word.charAt(k))
+                        break search;
+
+                for (int j = i; j < i+word.length(); ++j)
+                    mask[j] = true;
+            }
+
+        StringBuilder ans = new StringBuilder();
+        int anchor = 0;
+        for (int i = 0; i < N; ++i) {
+            if (mask[i] && (i == 0 || !mask[i-1]))
+                ans.append("<b>");
+            ans.append(S.charAt(i));
+            if (mask[i] && (i == N-1 || !mask[i+1]))
+                ans.append("</b>");
+        }
+        return ans.toString();
+    }
+
+    public boolean match(String S, int i, int j, String T) {
+        for (int k = i; k < j; ++k)
+            if (k >= S.length() || S.charAt(k) != T.charAt(k-i))
+                return false;
+        return true;
+    }
+}
+*/
--- problems/google/medium/addBoldTag.go
+++ problems/google/medium/addBoldTag.go
@@ -18,5 +18,61 @@ The given dict won't contain duplicates, and its length won't exceed 100.
 All the strings in input have length in range [1, 1000].
 */
 func addBoldTag(s string, dict []string) string {
+	if len(dict) == 0 {
+		return s
+	}
+	res := make([]string, 3*len(s))
+	closeSlice := make([][2]int, 0)
+	tagS := ""
+	first := [2]{strings.Index(s, dict[0]), strings.Index(s, dict[0])+len(dict[0])}
+	closeSlice = append(closeSlice, first)
+	// generate close position according to dict
+	for i :=1; i < len(dict); i++ {
+		for j :=0; j<len(closeSlice); j++ {
+			curStart := closeSlice[j][0]
+			curEnd := closeSlice[j][1]
+			start := strings.Index(s, dict[i])
+			end := strings.Index(s, dict[i])+len(dict[i])
+			//check [curStart, curEnd] overlap or sequence to [start, end]
+			if (start <= curEnd && end >= curStart  ) || start == curEnd+1 || end == curStart+1 {
+				closeSlice[j] = [2]int{min(start, curStart), max(end, curEnd)}
+			} else {
+				closeSlice = append(closeSlice, [2]int{start, end})
+			}
+		}
+	}
+	for i :=0; i<len(closeSlice); i++ {
+		start := closeSlice[i][0]
+		end := closeSlice[i][1]
+		res[start] = "<b>"
+		res[end] = "</b>"
+	}
+	for i :=0; i<len(s); i++ {
+		if res[i] == "" {
+			res[i] = string(s[i])
+		} else {
+			res[i+1] = string(s[i])
+		}
+	}
+	for i :=0; i<len(res); i++ {
+		if res[i] == "" {
+			break
+		}
+		tagS += res[i]
+	}
+	return tagS
+}
 
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
 }
