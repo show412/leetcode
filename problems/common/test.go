d3738fa618600e1b44fe7c55e0f6f3b8eafbf73d
hwsun <hwsun@freewheel.tv>
2019-07-13 16:50:53 +0800

    21Game don't understand

--- problems/common/test.go
+++ problems/common/test.go
@@ -2,68 +2,27 @@ package main
 
 import (
 	"fmt"
+	"math"
 	// "math"
 )
 
 func main() {
-	res := findLongestWord("abpcplea", []string{"a", "b", "c"})
+	res := new21Game(21, 17, 10)
 	fmt.Println(res)
 }
 
-func findLongestWord(s string, d []string) string {
-	if len(d) == 0 || len(s) == 0 {
-		return ""
-	}
-	candidateList := make([]string, 0)
-	// find which word is in s
-	for i := 0; i < len(d); i++ {
-		word := d[i]
-		pointS := 0
-		pointW := 0
-		for pointW < len(word) && pointS < len(s) {
-			if pointW == len(word)-1 && s[pointS] == word[pointW] {
-				candidateList = append(candidateList, word)
-				break
-			}
-			if s[pointS] == word[pointW] {
-				pointS++
-				pointW++
-				continue
-			} else {
-				if (len(s) - pointS) < (len(word) - pointW) {
-					break
-				}
-				pointS++
-			}
-		}
-	}
-	// find out the result
-	if len(candidateList) == 0 {
-		return ""
-	}
-	res := ""
-	for i := 0; i < len(candidateList); i++ {
-		word := candidateList[i]
-		if len(word) > len(res) {
-			res = word
-			continue
-		}
-		if len(word) == len(res) {
-			res = checkLex(word, res)
-		}
-	}
-	return res
-}
+func new21Game(N int, K int, W int) float64 {
+	// dp[x] = the answer when Alice has x points
+	dp := make([]float64, N+W+1)
 
-func checkLex(word1 string, word2 string) string {
-	s := 0
-	for s < len(word1) {
-		if word1[s] < word2[s] {
-			return word1
-		} else if word1[s] > word2[s] {
-			return word2
-		}
-		s++
+	for k := K; k <= N; k++ {
+		dp[k] = 1.0
 	}
-	return word1
+	// S = dp[k+1] + dp[k+2] + ... + dp[k+W]
+	S := math.Min(float64(N-K+1), float64(W))
+	for k := K - 1; k >= 0; k-- {
+		dp[k] = S / float64(W)
+		S += dp[k] - dp[k+W]
+	}
+	return dp[0]
 }
