743296608c3f533f2d8da19624c8064c8380d852
hwsun <hwsun@freewheel.tv>
2019-07-13 14:09:00 +0800

    findLongestWord with dictionary sort

--- problems/common/test.go
+++ problems/common/test.go
@@ -2,108 +2,68 @@ package main
 
 import (
 	"fmt"
-	"strings"
 	// "math"
 )
 
 func main() {
-	/*
-										Input:
-								s = "aaabbcc"
-								dict = ["aaa","aab","bc"]
-								Output:
-								"<b>aaabbc</b>c"
-
-										Input:
-								s = "abcxyz123"
-								dict = ["abc","123"]
-								Output:
-								"<b>abc</b>xyz<b>123</b>"
-
-								"aaabbcc"
-						["d"]
-
-				"zxioozhirtosxhlkaeociwdsgyqnvxuxizhvfrkestehiiwpmayhlyybhnpgfcxfjltnhlwppzfhxyozlaorrcfdhpbjxwalxtxutnperhobhatxwnvowtolsbsxfwzpyttpksrhhkvtfycpuvztjeeabraqqwustqubzzmjkuzafwcuixdbiuwqgexmlqzlyjxdwjzwjuxopecpcbojkrrceysozvkabzirctyroynqbyomecatnjupiqjexevltvtspnkpkzajzqcagsqnkagrludmpsgczbtkvbbcbjenacgrtowsjxazboxcjdhvjnhcaqsjokztkskntlwpnrecnrfuhuseuhshqoddhutcocgrbvnxgnliiinbjcoydtdlyvfjlmpzegscxcfoacxhwaqusneoptvmfrbljkpsbixsjlbvatkyoillejllsvlqfuvxqaareawgfwwultacnjgepmzqhykqalbqfhxohzfntgiatttqupukfhcvghqjzrutxlwidbfqrssrrnbtbjnsggdvrxrbotvwpofpxrwgcecbcqczuuxlrfpnskznmjdcbqktilxijkilpjwywplxdnirjhgoisfwamuauljoqzbmxlvtzdqocbvrusvxyvslufvbcigggddwlubnjjlxmmmcrcefssuqwvtvmwkhovflsxxraneluwdraknumssfluujdlayviukvaqgumpdefzivsqpjkymhlkltojsbyzmvyusmnuxytexhjcszblvimywfoqwsihtchhqnotxvibjzqhthhojreuuknhbfhzmivqnbofrwktfwlcbajoygobbesqeeiemullkrqwplsmsuvskuifbojrdvtffpqm"
-				["zx","oo","hi","to","xh","ka","oc","wd","gy","nv","ux","zh","fr","es","eh","iw","ma","hl","yb","np","fc","fj","tn","lw","pz","hx","oz","ao","rc","dh","bj"]
-
-		"aaabbcc"
-		["a","b","c"]
-
-	*/
-
-	// res := addBoldTag("zxioozhirtosxhlkaeociwdsgyqnvxuxizhvfrkestehiiwpmayhlyybhnpgfcxfjltnhlwppzfhxyozlaorrcfdhpbjxwalxtxutnperhobhatxwnvowtolsbsxfwzpyttpksrhhkvtfycpuvztjeeabraqqwustqubzzmjkuzafwcuixdbiuwqgexmlqzlyjxdwjzwjuxopecpcbojkrrceysozvkabzirctyroynqbyomecatnjupiqjexevltvtspnkpkzajzqcagsqnkagrludmpsgczbtkvbbcbjenacgrtowsjxazboxcjdhvjnhcaqsjokztkskntlwpnrecnrfuhuseuhshqoddhutcocgrbvnxgnliiinbjcoydtdlyvfjlmpzegscxcfoacxhwaqusneoptvmfrbljkpsbixsjlbvatkyoillejllsvlqfuvxqaareawgfwwultacnjgepmzqhykqalbqfhxohzfntgiatttqupukfhcvghqjzrutxlwidbfqrssrrnbtbjnsggdvrxrbotvwpofpxrwgcecbcqczuuxlrfpnskznmjdcbqktilxijkilpjwywplxdnirjhgoisfwamuauljoqzbmxlvtzdqocbvrusvxyvslufvbcigggddwlubnjjlxmmmcrcefssuqwvtvmwkhovflsxxraneluwdraknumssfluujdlayviukvaqgumpdefzivsqpjkymhlkltojsbyzmvyusmnuxytexhjcszblvimywfoqwsihtchhqnotxvibjzqhthhojreuuknhbfhzmivqnbofrwktfwlcbajoygobbesqeeiemullkrqwplsmsuvskuifbojrdvtffpqm", []string{"zx", "oo", "hi", "to", "xh", "ka", "oc", "wd", "gy", "nv", "ux", "zh", "fr", "es", "eh", "iw", "ma", "hl", "yb", "np", "fc", "fj", "tn", "lw", "pz", "hx", "oz", "ao", "rc", "dh", "bj"})
-	res := addBoldTag("abcxyz123", []string{"abc", "123"})
+	res := findLongestWord("abpcplea", []string{"a", "b", "c"})
 	fmt.Println(res)
 }
 
-func addBoldTag(s string, dict []string) string {
-	if len(dict) == 0 {
-		return s
+func findLongestWord(s string, d []string) string {
+	if len(d) == 0 || len(s) == 0 {
+		return ""
 	}
-	res := make([]string, 0)
-	bold := make(map[int]bool)
-	end := 0
-	for i := 0; i < len(s); i++ {
-		for j := 0; j < len(dict); j++ {
-			if (i+len(dict[j]) <= len(s)) && s[i:(i+len(dict[j]))] == dict[j] {
-				end = max(end, i+len(dict[j]))
+	candidateList := make([]string, 0)
+	// find which word is in s
+	for i := 0; i < len(d); i++ {
+		word := d[i]
+		pointS := 0
+		pointW := 0
+		for pointW < len(word) && pointS < len(s) {
+			if pointW == len(word)-1 && s[pointS] == word[pointW] {
+				candidateList = append(candidateList, word)
+				break
+			}
+			if s[pointS] == word[pointW] {
+				pointS++
+				pointW++
+				continue
+			} else {
+				if (len(s) - pointS) < (len(word) - pointW) {
+					break
+				}
+				pointS++
 			}
 		}
-		if end > i {
-			bold[i] = true
-		}
-
 	}
-
-	for i := 0; i < len(s); i++ {
-		if _, ok := bold[i]; !ok {
-			res = append(res, string(s[i]))
+	// find out the result
+	if len(candidateList) == 0 {
+		return ""
+	}
+	res := ""
+	for i := 0; i < len(candidateList); i++ {
+		word := candidateList[i]
+		if len(word) > len(res) {
+			res = word
 			continue
 		}
-		j := i
-		for j < len(s) && bold[j] == true {
-			j++
+		if len(word) == len(res) {
+			res = checkLex(word, res)
 		}
-
-		res = append(res, "<b>"+s[i:j]+"</b>")
-		i = j - 1
 	}
-	return strings.Join(res, "")
+	return res
 }
 
-func max(a, b int) int {
-	if a > b {
-		return a
+func checkLex(word1 string, word2 string) string {
+	s := 0
+	for s < len(word1) {
+		if word1[s] < word2[s] {
+			return word1
+		} else if word1[s] > word2[s] {
+			return word2
+		}
+		s++
 	}
-	return b
+	return word1
 }
-
-/*
-public class Solution {
-    public String addBoldTag(String s, String[] dict) {
-        boolean[] bold = new boolean[s.length()];
-        for (int i = 0, end = 0; i < s.length(); i++) {
-            for (String word : dict) {
-                if (s.startsWith(word, i)) {
-                    end = Math.max(end, i + word.length());
-                }
-            }
-            bold[i] = end > i;
-        }
-
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            if (!bold[i]) {
-                result.append(s.charAt(i));
-                continue;
-            }
-            int j = i;
-            while (j < s.length() && bold[j]) j++;
-            result.append("<b>" + s.substring(i, j) + "</b>");
-            i = j - 1;
-        }
-
-        return result.toString();
-    }
-}
-*/
