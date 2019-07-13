ec5e93828f7b8754090d7866866839fbc3339660
hwsun <hwsun@freewheel.tv>
2019-07-13 17:08:07 +0800

    rewrite with golang

---
+++ problems/easy/reverse_list.go
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/reverse-linked-list/description/
+/*
+Reverse a singly linked list.
+
+Example:
+
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+Follow up:
+
+A linked list can be reversed either iteratively or recursively. Could you implement both?
+*/
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+	var pre ListNode
+	cur := head
+	for cur != nil {
+		nextNode := cur.Next
+		cur.Next = *pre
+		pre = cur
+		cur = nextNode
+	}
+	return pre
+}
