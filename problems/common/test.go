b7c82a4b8a46fdf9e5b369a816add674b53a30df
hwsun <hwsun@freewheel.tv>
2019-07-20 00:30:18 +0800

    canTransform solution with golang

--- problems/common/test.go
+++ problems/common/test.go
@@ -11,7 +11,7 @@ test case:
 "bbbbaaaaababaababab"
 */
 func main() {
-	res := canTransform("XXXXXLXXXX", "LXXXXXXXXX")
+	res := canTransform("XLLR", "LXLX")
 	fmt.Println(res)
 }
 
@@ -23,26 +23,25 @@ func canTransform(start string, end string) bool {
 			return false
 		}
 	}
-	m := map[string]string{"XL": "LX", "RX": "XR"}
-	for i := 0; i < len(start)-1; i++ {
-		if start[i] == end[i] {
-			continue
-		} else {
-			s := string(start[i]) + string(start[i+1])
-			e := string(end[i]) + string(end[i+1])
-			fmt.Println("*****")
-			fmt.Println(i)
-			fmt.Println(s)
-			fmt.Println(e)
-			if v, _ := m[s]; v == e {
-				i++
-			} else {
+	i := 0
+	j := 0
+	for i < len(start) && j < len(start) {
+		for i < len(start) && start[i] == 'X' {
+			i++
+		}
+		for j < len(start) && end[j] == 'X' {
+			j++
+		}
+		if (i < len(start) && j >= len(start)) || (i >= len(start) && j < len(start)) {
+			return false
+		}
+		if i < len(start) && j < len(start) {
+			if start[i] != end[j] || (start[i] == 'L' && i < j) || (start[i] == 'R' && i > j) {
 				return false
 			}
 		}
-	}
-	if start[len(start)-1] != end[len(end)-1] {
-		return false
+		i++
+		j++
 	}
 	return true
 }
--- problems/google/medium/canTransform.go
+++ problems/google/medium/canTransform.go
@@ -23,6 +23,17 @@ test case:
 	start = "RXXLRXRXL", end = "XRLXXRRLX" true
 	"XLLR"  "LXLX"  false
 	"XXXXXLXXXX" "LXXXXXXXXX" true
+	"XXXLXXXXXX" "XXXLXXXXXX" true
+*/
+// not from left to right
+// solution https://leetcode.com/problems/swap-adjacent-in-lr-string/solution/
+/*
+L postion in start must be not smaller than L postion in end, becasue L only move to left
+R postion in start must be not bigger than R postion in end, becasue R only move to right
+It's the sufficent condition for this problem
+
+Time Complexity: O(N)O(N), where NN is the length of start and end.
+Space Complexity: O(1)O(1).
 */
 func canTransform(start string, end string) bool {
 	if len(start) == 1 {
@@ -32,19 +43,26 @@ func canTransform(start string, end string) bool {
 			return false
 		}
 	}
-	m := map[string]string{"XL": "LX", "RX": "XR"}
-	for i := 0; i < len(start)-1; i++ {
-		if start[i] == end[i] {
-			continue
-		} else {
-			s := string(start[i]) + string(start[i+1])
-			e := string(end[i]) + string(end[i+1])
-			if v, _ := m[s]; v == e {
-				i++
-			} else {
+	i := 0
+	j := 0
+	for i < len(start) && j < len(start) {
+		for i < len(start) && start[i] == 'X' {
+			i++
+		}
+		for j < len(start) && end[j] == 'X' {
+			j++
+		}
+		// it means the count of L or count of R in start and end have not the same.
+		if (i < len(start) && j >= len(start)) || (i >= len(start) && j < len(start)) {
+			return false
+		}
+		if i < len(start) && j < len(start) {
+			if start[i] != end[j] || (start[i] == 'L' && i < j) || (start[i] == 'R' && i > j) {
 				return false
 			}
 		}
+		i++
+		j++
 	}
 	return true
 }
