267a3ce9382a966fb2efca7d8c1a70ead758da77
hwsun <hwsun@freewheel.tv>
2019-07-22 23:34:42 +0800

    minDominoRatation solution with go

--- problems/common/test.go
+++ problems/common/test.go
@@ -26,53 +26,49 @@ func minDominoRotations(A []int, B []int) int {
 	resAB := 0
 	flagA := false
 	// A[0] is the standard
-	for i := 1; i < len(A); i++ {
-		if A[i] == A[0] {
-			flagA = true
-			resAB++
-			continue
-		} else if B[i] == A[0] {
-			resA++
-		} else {
+	for i := 0; i < len(A); i++ {
+		if A[i] != A[0] && B[i] != A[0] {
 			flagA = false
 			break
+		} else if B[i] != A[0] {
+			resAB++
+			// continue
+		} else if A[i] != A[0] {
+			resA++
 		}
 		flagA = true
 	}
-	fmt.Println(resA)
-	fmt.Println(resAB)
-	ra := min(resA, resAB)
+
+	resA = min(resA, resAB)
+
 	// B[0] is the standard
 	resB := 0
 	resBA := 0
 	flagB := false
 	// A[0] is the standard
-	for i := 1; i < len(B); i++ {
-		if B[i] == B[0] {
-			flagB = true
-			resBA++
-			continue
-		} else if A[i] == B[0] {
-			resB++
-		} else {
+	for i := 0; i < len(B); i++ {
+		if A[i] != B[0] && B[i] != B[0] {
 			flagB = false
 			break
+		} else if B[i] != B[0] {
+			resBA++
+		} else if A[i] != B[0] {
+			resB++
 		}
 		flagB = true
 	}
-	fmt.Println(resB)
-	// fmt.Println(len(B))
-	rb := min(resB, resBA)
+
+	resB = min(resB, resBA)
 	if flagA == false && flagB == false {
 		return -1
 	}
 	if flagA == true && flagB == true {
-		return min(ra, rb)
+		return min(resA, resB)
 	}
 	if flagA == true {
-		return ra
+		return resA
 	}
-	return rb
+	return resB
 }
 func min(a, b int) int {
 	if a < b {
