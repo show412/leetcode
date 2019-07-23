b030120d68386558ee8239af168f991e631acb04
hwsun <hwsun@freewheel.tv>
2019-07-24 00:13:01 +0800

    handle parenthesis with divided conquer

--- problems/google/medium/diffWaysToCompute.go
+++ problems/google/medium/diffWaysToCompute.go
@@ -1,33 +1,39 @@
-package main
+import "strconv"
 
-import (
-	"fmt"
-	"strconv"
-	// "math"
-)
-
+// https://leetcode.com/problems/different-ways-to-add-parentheses/
 /*
-test case:
-"kkkkzrkatkwpkkkktrq"
-"bbbbaaaaababaababab"
-*/
-func main() {
-	res := diffWaysToCompute("2*3-4*5")
-	fmt.Println(res)
-}
+Given a string of numbers and operators, return all possible results from computing all the different possible ways to group numbers and operators. The valid operators are +, - and *.
 
+Example 1:
+
+Input: "2-1-1"
+Output: [0, 2]
+Explanation:
+((2-1)-1) = 0
+(2-(1-1)) = 2
+Example 2:
+
+Input: "2*3-4*5"
+Output: [-34, -14, -10, -10, 10]
+Explanation:
+(2*(3-(4*5))) = -34
+((2*3)-(4*5)) = -14
+((2*(3-4))*5) = -10
+(2*((3-4)*5)) = -10
+(((2*3)-4)*5) = 10
+*/
+/*
+It's a divided-conquer, all possibile is divided int two parts
+then conquer each result
+*/
 func diffWaysToCompute(input string) []int {
 	res := make([]int, 0)
 	for i := 0; i < len(input); i++ {
 		if input[i] == '-' || input[i] == '+' || input[i] == '*' {
 			part1 := ([]byte(input))[:i]
 			part2 := ([]byte(input))[(i + 1):]
-			// fmt.Println(string(part1))
 			part1Res := diffWaysToCompute(string(part1))
 			part2Res := diffWaysToCompute(string(part2))
-			// fmt.Println("****")
-			// fmt.Println(part1Res)
-			// fmt.Println(part2Res)
 			for _, p1 := range part1Res {
 				for _, p2 := range part2Res {
 					c := 0
@@ -44,12 +50,9 @@ func diffWaysToCompute(input string) []int {
 		}
 	}
 	if len(res) == 0 {
-		// fmt.Printf("%T, %v", input, input)
 		v, _ := strconv.Atoi(input)
-		// fmt.Printf("%T, %v", v, v)
 		res = append(res, v)
-		// return res
-		// fmt.Println(res)
 	}
 	return res
 }
+
--- problems/google/medium/generateParenthesis.go
+++ problems/google/medium/generateParenthesis.go
@@ -16,6 +16,7 @@ import "strconv"
 // solution of answer
 // because only the open bracket size is bigger than the close bracket size
 // it will be reseanable
+
 func generateParenthesis(n int) []string {
 	if n == 0 {
 		return []string{}
@@ -38,6 +39,22 @@ func recursionAdd(n int, str string, open int, close int, res *[]string) {
 		recursionAdd(n, str+")", open, close+1, res)
 	}
 	return
+}
+
+// the solution is like divided conquer
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList();
+        if (n == 0) {
+            ans.add("");
+        } else {
+            for (int c = 0; c < n; ++c)
+                for (String left: generateParenthesis(c))
+                    for (String right: generateParenthesis(n-1-c))
+                        ans.add("(" + left + ")" + right);
+        }
+        return ans;
+    }
 }
 
 // DFS like permutation
