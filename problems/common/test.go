19e2e12cacbcc9084c12002453ddf10d2ce26251
hwsun <hwsun@freewheel.tv>
2019-07-29 13:31:28 +0800

    add some comments for expressiveWords

--- problems/common/test.go
+++ problems/common/test.go
@@ -11,58 +11,73 @@ test case:
 "bbbbaaaaababaababab"
 */
 func main() {
-	res := shipWithinDays([]int{3, 2, 2, 4, 1, 4}, 3)
+	res := expressiveWords("heeellooo", []string{"hello", "hi", "helo"})
 	fmt.Println(res)
 }
 
-func shipWithinDays(weights []int, D int) int {
-	// res := 0
-	// frontSum := make([]int, len(weights))
-	// frontSum[0] = weights[0]
-	start := 0
-	end := 0
-	for i := 0; i < len(weights); i++ {
-		start = max(start, weights[i])
-		end += weights[i]
+func expressiveWords(S string, words []string) int {
+	if len(words) == 0 {
+		return 0
 	}
-	if D == 1 {
-		return end
-	}
-	for start < end {
-		mid := start + (end-start)/2
-		if checkCapacity(weights, mid, D) > 0 {
-			start = mid + 1
-		} else {
-			end = mid
-		}
+	res := 0
+	SByteArray, SByteNum := generatePair(S)
+	fmt.Println(SByteArray)
+	fmt.Println(SByteNum)
+	for i := 0; i < len(words); i++ {
+		word := words[i]
+		wordByteArray, wordByteNum := generatePair(word)
+		fmt.Println(wordByteArray)
+		fmt.Println(wordByteNum)
+		if sliceEqual(SByteArray, wordByteArray) == true {
+			for j := 0; j < len(wordByteNum); j++ {
+				if (SByteNum[j] < 3 && SByteNum[j] != wordByteNum[j]) || SByteNum[j] < wordByteNum[j] {
+					break
+				}
 
-	}
-	return start
-}
+				if j == len(wordByteNum)-1 {
+					res++
+				}
 
-func checkCapacity(weights []int, capacity int, D int) int {
-	count := 1
-	sum := 0
-	// fmt.Println(capacity)
-	for i := 0; i < len(weights); i++ {
-		if (sum + weights[i]) > capacity {
-			count++
-			sum = weights[i]
-		} else {
-			sum += weights[i]
+			}
 		}
 	}
-	if count > D {
-		return 1
-	} else if count < D {
-		return -1
-	}
-	return 0
+	return res
 }
 
-func max(a, b int) int {
-	if a > b {
-		return a
+func generatePair(s string) ([]byte, []int) {
+	var byteArray []byte
+	var byteNum []int
+	byteArray = append(byteArray, s[0])
+	num := 1
+	for i := 1; i < len(s); i++ {
+		if s[i] == s[i-1] {
+			num++
+		} else {
+			byteArray = append(byteArray, s[i])
+			byteNum = append(byteNum, num)
+			num = 1
+		}
+		if i == len(s)-1 {
+			byteNum = append(byteNum, num)
+		}
 	}
-	return b
+	return byteArray, byteNum
+}
+
+func sliceEqual(s1, s2 []byte) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+
+	if (s1 == nil) != (s2 == nil) {
+		return false
+	}
+
+	for i, v := range s1 {
+		if v != s2[i] {
+			return false
+		}
+	}
+
+	return true
 }
