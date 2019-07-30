ec28f72258a129714523c846e6a502ac1d9cbf9e
hwsun <hwsun@freewheel.tv>
2019-07-30 17:08:50 +0800

    add some comments for the option

--- problems/google/medium/SumRegion.go
+++ problems/google/medium/SumRegion.go
@@ -65,6 +65,8 @@ func Constructor(matrix [][]int) NumMatrix {
 	for i := 0; i < len(dp); i++ {
 		dp[i] = make([]int, len(matrix[0])+1)
 	}
+	// dp[i+1][j+1] represents the sum of area from matrix[0][0] to matrix[i][j]
+	// 不能用 dp[r][c]代表 因为一减会导致边被减去
 	for r := 0; r < len(matrix); r++ {
 		for c := 0; c < len(matrix[0]); c++ {
 			dp[r+1][c+1] = dp[r+1][c] + dp[r][c+1] + matrix[r][c] - dp[r][c]
@@ -75,6 +77,62 @@ func Constructor(matrix [][]int) NumMatrix {
 
 func (this *NumMatrix) SumRegion(row1 int, col1 int, row2 int, col2 int) int {
 	return this.dp[row2+1][col2+1] - this.dp[row1][col2+1] - this.dp[row2+1][col1] + this.dp[row1][col1]
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * obj := Constructor(matrix);
+ * param_1 := obj.SumRegion(row1,col1,row2,col2);
+ */
+
+/*
+ 上面的 row2+1 col2+1 不太好理解 也可以用 dp[r][c]代表 rc 的sum 和
+ 这个时候就要考虑边界情况了 各种 edge case 不太好处理
+ 各种+1的情况就好办了 因为初始化的时候 默认都是0 不会有 key 小于0的情况 而且没初始化的默认就是0
+*/
+
+type NumMatrix struct {
+	dp [][]int
+}
+
+func Constructor(matrix [][]int) NumMatrix {
+	if len(matrix) == 0 || len(matrix[0]) == 0 {
+		return NumMatrix{dp: [][]int{}}
+	}
+	dp := make([][]int, len(matrix)+1)
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, len(matrix[0])+1)
+	}
+	dp[0][0] = matrix[0][0]
+	if len(matrix[0]) > 1 {
+		dp[0][1] = matrix[0][0] + matrix[0][1]
+	} else {
+		dp[0][1] = 0
+	}
+	if len(matrix) > 1 {
+		dp[1][0] = matrix[0][0] + matrix[1][0]
+	} else {
+		dp[1][0] = 0
+	}
+	for r := 1; r < len(matrix); r++ {
+		for c := 1; c < len(matrix[0]); c++ {
+			dp[r][c] = dp[r-1][c] + dp[r][c-1] + matrix[r][c] - dp[r-1][c-1]
+		}
+	}
+	return NumMatrix{dp: dp}
+}
+
+func (this *NumMatrix) SumRegion(row1 int, col1 int, row2 int, col2 int) int {
+	if row1 == 0 && col1 > 0 {
+		return this.dp[row2][col2] - this.dp[0][col2] - this.dp[row2][col1-1] + this.dp[0][col1-1]
+	}
+	if row1 > 0 && col1 == 0 {
+		return this.dp[row2][col2] - this.dp[row1-1][col2] - this.dp[row2][0] + this.dp[row1-1][0]
+	}
+	if row1 == 0 && col1 == 0 {
+		return this.dp[row2][col2]
+	}
+	return this.dp[row2][col2] - this.dp[row1-1][col2] - this.dp[row2][col1-1] + this.dp[row1-1][col1-1]
 }
 
 /**
