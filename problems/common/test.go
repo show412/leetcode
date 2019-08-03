88ef6ef49a8cd84c1eb46f20a39f4d222119bdea
hwsun <hwsun@freewheel.tv>
2019-08-03 21:59:21 +0800

    isomorphic-strings solution

--- problems/common/test.go
+++ problems/common/test.go
@@ -11,59 +11,30 @@ test case:
 "bbbbaaaaababaababab"
 */
 func main() {
-	res := searchRange([]int{5, 7, 7, 8, 8, 10}, 8)
+	res := isIsomorphic("paper", "title")
 	fmt.Println(res)
 }
 
-func searchRange(nums []int, target int) []int {
-	if len(nums) == 0 {
-		return []int{-1, -1}
+func isIsomorphic(s string, t string) bool {
+	l := len(s)
+	if l == 0 {
+		return true
 	}
-	res := make([]int, 2)
-	start := 0
-	end := len(nums) - 1
-	// find the boundary left
-	for start+1 < end {
-		mid := start + (end-start)/2
-		if nums[mid] > target {
-			end = mid
-		} else if nums[mid] == target {
-			// because we need to find the left boundary, so it needs to find the small range
-			// so end = mid there
-			end = mid
+	ms := make(map[byte]byte, 0)
+	mt := make(map[byte]byte, 0)
+	for i := 0; i < l; i++ {
+		vs, oks := ms[s[i]]
+		vt, okt := mt[t[i]]
+		if !oks && !okt {
+			ms[s[i]] = t[i]
+			mt[t[i]] = s[i]
+		} else if oks && okt {
+			if vs != t[i] || vt != s[i] {
+				return false
+			}
 		} else {
-			start = mid
+			return false
 		}
 	}
-	if nums[start] == target {
-		res[0] = start
-	} else if nums[end] == target {
-		res[0] = end
-	} else {
-		return []int{-1, -1}
-	}
-
-	// find the boundary right
-	start = 0
-	end = len(nums) - 1
-	for start+1 < end {
-		mid := start + (end-start)/2
-		if nums[mid] > target {
-			end = mid
-		} else if nums[mid] == target {
-			// because we need to find the right boundary, so it needs to find the big range
-			// so start = mid there
-			start = mid
-		} else {
-			start = mid
-		}
-	}
-	if nums[end] == target {
-		res[1] = end
-	} else if nums[start] == target {
-		res[1] = start
-	} else {
-		return []int{-1, -1}
-	}
-	return res
+	return true
 }
--- problems/google/easy/isomorphic-strings.go
+++ problems/google/easy/isomorphic-strings.go
@@ -22,5 +22,25 @@ Note:
 You may assume both s and t have the same length.
 */
 func isIsomorphic(s string, t string) bool {
-
+	l := len(s)
+	if l == 0 {
+		return true
+	}
+	ms := make(map[byte]byte, 0)
+	mt := make(map[byte]byte, 0)
+	for i := 0; i < l; i++ {
+		vs, oks := ms[s[i]]
+		vt, okt := mt[t[i]]
+		if !oks && !okt {
+			ms[s[i]] = t[i]
+			mt[t[i]] = s[i]
+		} else if oks && okt {
+			if vs != t[i] || vt != s[i] {
+				return false
+			}
+		} else {
+			return false
+		}
+	}
+	return true
 }
