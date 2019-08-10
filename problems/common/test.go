648c22ae5873784bbdb39b5d4b68f1d8ea8aae80
hwsun <hwsun@freewheel.tv>
2019-08-10 16:48:21 +0800

    productExceptSelf.go

--- problems/common/test.go
+++ problems/common/test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"sort"
 	// "math"
 )
 
@@ -12,42 +11,33 @@ test case:
 "bbbbaaaaababaababab"
 */
 func main() {
-	res := reconstructQueue([][]int{{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}})
+	res := productExceptSelf([]int{1, 2, 3, 4})
 	fmt.Println(res)
 }
 
-func reconstructQueue(people [][]int) [][]int {
-	if len(people) == 0 {
-		return people
-	}
-	res := make([][]int, len(people))
-	// sort.Sort should use the defined type
-	sort.Sort(peopleSlice(people))
-	fmt.Println(people)
-	for i := 0; i < len(people); i++ {
-		start := 0
-		for j := 0; j < len(res); j++ {
-			if res[j] != nil {
-				continue
-			}
-			if start == people[i][1] {
-				res[j] = people[i]
-			}
-			fmt.Println(res)
-			start++
+func productExceptSelf(nums []int) []int {
+	res := make([]int, len(nums))
+	prefix := make([]int, len(nums))
+	suffix := make([]int, len(nums))
+	for i := 0; i < len(nums); i++ {
+		product := 1
+		if i != 0 {
+			product = prefix[i-1] * nums[i-1]
 		}
+		prefix[i] = product
 	}
+	fmt.Println(prefix)
+	for i := len(nums) - 1; i >= 0; i-- {
+		product := 1
+		if i != len(nums)-1 {
+			product = suffix[i+1] * nums[i+1]
+		}
+		suffix[i] = product
+	}
+	fmt.Println(suffix)
+	for i := 0; i < len(nums); i++ {
+		res[i] = prefix[i] * suffix[i]
+	}
+
 	return res
-}
-
-type peopleSlice [][]int
-
-func (a peopleSlice) Len() int {
-	return len(a)
-}
-func (a peopleSlice) Less(i, j int) bool {
-	return a[i][0] < a[j][0] || (a[i][0] == a[j][0] && a[i][1] > a[j][1])
-}
-func (a peopleSlice) Swap(i, j int) {
-	a[i], a[j] = a[j], a[i]
 }
