7d8b3e21039f9cf5057b816ed5a16dd5645813f4
hwsun <hwsun@freewheel.tv>
2019-08-12 22:53:22 +0800

    addBinary.go solution

--- problems/facebook/easy/addBinary.go
+++ problems/facebook/easy/addBinary.go
@@ -1,20 +1,18 @@
-package main
-
-import (
-	"fmt"
-	// "math"
-)
-
+// https://leetcode.com/problems/add-binary/
 /*
-test case:
-"kkkkzrkatkwpkkkktrq"
-"bbbbaaaaababaababab"
-*/
-func main() {
-	res := addBinary("0", "0")
-	fmt.Println(res)
-}
+Given two binary strings, return their sum (also a binary string).
 
+The input strings are both non-empty and contains only characters 1 or 0.
+
+Example 1:
+
+Input: a = "11", b = "1"
+Output: "100"
+Example 2:
+
+Input: a = "1010", b = "1011"
+Output: "10101"
+*/
 func addBinary(a string, b string) string {
 	i := len(a) - 1
 	j := len(b) - 1
@@ -66,3 +64,4 @@ func max(a, b int) int {
 	}
 	return b
 }
+
