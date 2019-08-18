a107f6c2ee3299673a621cdb59f6ed5c61f02386
hwsun <hwsun@freewheel.tv>
2019-08-18 21:28:16 +0800

    solution of tree to DoublyList JS with recursion

--- problems/facebook/medium/treeToDoublyList.js
+++ problems/facebook/medium/treeToDoublyList.js
@@ -36,6 +36,33 @@ while the dashed line means the predecessor relationship.
  * @param {Node} root
  * @return {Node}
  */
+// refer to
+// https://leetcode.com/problems/convert-binary-search-tree-to-sorted-doubly-linked-list/solution/
 var treeToDoublyList = function (root) {
+  let head = null;
+  let prev;
 
+  if (!root) { return root }
+
+  function inOrder(node) {
+    if (!node) { return; }
+
+    inOrder(node.left);
+
+    if (!head) {
+      head = node; prev = node
+    } else {
+      node.left = prev;
+      prev.right = node;
+      prev = node;
+    }
+
+    inOrder(node.right);
+  }
+
+  inOrder(root);
+  prev.right = head;
+  head.left = prev;
+
+  return head;
 };
