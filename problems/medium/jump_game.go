afbdbc5d7a5312d2d0a7d72b3911e227296708f1
hwsun <hwsun@freewheel.tv>
2019-09-03 17:43:06 +0800

    jumpgame solution

---
+++ problems/medium/jump_game.go
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/jump-game/
+/*
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Determine if you are able to reach the last index.
+
+Example 1:
+
+Input: [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum
+             jump length is 0, which makes it impossible to reach the last index.
+*/
+func canJump(nums []int) bool {
+	f := make([]bool, len(nums))
+	f[0] = true
+	for i := 1; i < len(nums); i++ {
+		for j := 0; j < i; j++ {
+			f[i] = f[j] && nums[j] >= (i-j)
+			if f[i] == true {
+				break
+			}
+		}
+	}
+	return f[len(nums)-1]
+}
