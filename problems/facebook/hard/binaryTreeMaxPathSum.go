edac45a6b6c7b5efbacbd1f228013abc19b7862d
hwsun <hwsun@freewheel.tv>
2019-09-06 09:36:31 +0800

    BinaryTreeMaxPath solution

--- problems/facebook/hard/binaryTreeMaxPathSum.go
+++ problems/facebook/hard/binaryTreeMaxPathSum.go
@@ -1,23 +1,34 @@
-// https://www.lintcode.com/problem/binary-tree-maximum-path-sum/description
-// Given a binary tree, find the maximum path sum.
+import "math"
 
-// The path may start and end at any node in the tree.
+// https://leetcode.com/problems/binary-tree-maximum-path-sum/
+/*
+Given a non-empty binary tree, find the maximum path sum.
 
-// Example
-// Example 1:
-// 	Input:  For the following binary tree（only one node）:
-// 	2
-// 	Output：2
+For this problem, a path is defined as any sequence of nodes from some starting node
+to any node in the tree along the parent-child connections.
+The path must contain at least one node and does not need to go through the root.
 
-// Example 2:
-// 	Input:  For the following binary tree:
+Example 1:
 
-//       1
-//      / \
-//     2   3
+Input: [1,2,3]
 
-// 	Output: 6
+       1
+      / \
+     2   3
 
+Output: 6
+Example 2:
+
+Input: [-10,9,20,null,null,15,7]
+
+   -10
+   / \
+  9  20
+    /  \
+   15   7
+
+Output: 42
+*/
 /**
  * Definition for a binary tree node.
  * type TreeNode struct {
@@ -27,26 +38,23 @@
  * }
  */
 
-/**
- * @param root: The root of binary tree.
- * @return: An integer
- */
 // singlePath means the max path to be through root
-// maxPath means the max path unnessary to be through root, but at least one node here
+// maxPath means the max path unnessary to be through root,but at least one node here
 type ResultType struct {
 	singlePath int
 	maxPath    int
 }
 
-const INT_MAX = int(^uint(0) >> 1)
-const INT_MIN = ^INT_MAX
+const INT_MIN = math.MinInt64
 
 func helper(root *TreeNode) ResultType {
 	if root == nil {
 		return ResultType{singlePath: INT_MIN, maxPath: INT_MIN}
 	}
+	// Divide
 	left := helper(root.Left)
 	right := helper(root.Right)
+	// Conquer
 	singlePath := max(0, max(left.singlePath, right.singlePath)) + root.Val
 	maxPath := max(left.maxPath, right.maxPath)
 	maxPath = max(maxPath, max(left.singlePath, 0)+max(right.singlePath, 0)+root.Val)
