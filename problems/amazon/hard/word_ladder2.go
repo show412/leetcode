1443948cd06519cb520b8c903f851fe21160c55d
hwsun <hwsun@freewheel.tv>
2019-09-14 16:05:01 +0800

    word ladder 2 BFS+DFS and TBD with golang

--- problems/amazon/hard/word_ladder2.go
+++ problems/amazon/hard/word_ladder2.go
@@ -35,6 +35,7 @@ Output: []
 Explanation: The endWord "cog" is not in wordList, therefore no possible transformation.
 
 */
+// 这个不是很好理解 有些地方并不知道为什么 下面的java的方法是常规的实现方法
 func findLadders(beginWord string, endWord string, wordList []string) [][]string {
 
 	result := make([][]string, 0)
@@ -107,4 +108,84 @@ func findLadders(beginWord string, endWord string, wordList []string) [][]string
 	}
 
 	return result
+}
+
+
+// 先构建图再找图中最小路径 BFS+DFS 注意BFS里用hash减少时间复杂度
+class Solution {
+    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        List<String> path = new ArrayList<>();
+        List<List<String>> result = new ArrayList<List<String>>();
+        HashMap<String, List<String>> graph = new HashMap<String, List<String>>();
+        HashSet<String> dict = new HashSet<>(wordList);
+        buildGraph(beginWord, endWord, graph, dict);
+        dfs(beginWord, endWord, graph, path, result);
+        return result;
+    }
+
+    private void buildGraph(String beginWord, String endWord, HashMap<String, List<String>> graph, HashSet<String> dict) {
+        HashSet<String> visited = new HashSet<>();
+        HashSet<String> toVisit = new HashSet<>();
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+        toVisit.add(beginWord);
+        boolean foundEnd = false;
+
+        while(!queue.isEmpty()) {
+            visited.addAll(toVisit);
+            toVisit.clear();
+            int count = queue.size();
+
+            for (int i = 0; i < count; i++) {
+                String word = queue.poll();
+                List<String> children = getNextLevel(word, dict);
+                for (String child : children) {
+                    if (child.equals(endWord)) foundEnd = true;
+                    if (!visited.contains(child)) {
+                        if (!graph.containsKey(word)) {
+                            graph.put(word, new ArrayList<String>());
+                        }
+                        graph.get(word).add(child);
+                    }
+                    if (!visited.contains(child) && !toVisit.contains(child)) {
+                        queue.offer(child);
+                        toVisit.add(child);
+                    }
+                }
+            }
+
+            if (foundEnd) break;
+        }
+    }
+
+    private List<String> getNextLevel(String word, HashSet<String> dict) {
+        List<String> result = new ArrayList<>();
+        char[] chs = word.toCharArray();
+
+        for (int i = 0; i < chs.length; i++) {
+            for (char c = 'a'; c <= 'z'; c++) {
+                if (chs[i] == c) continue;
+                char t = chs[i];
+                chs[i] = c;
+                String target = String.valueOf(chs);
+                if (dict.contains(target)) result.add(target);
+                chs[i] = t;
+            }
+        }
+
+        return result;
+    }
+
+    private void dfs(String curWord, String endWord, HashMap<String, List<String>> graph, List<String> path, List<List<String>> result) {
+        path.add(curWord);
+
+        if (curWord.equals(endWord)) result.add(new ArrayList<String>(path));
+        else if (graph.containsKey(curWord)) {
+            for (String nextWord : graph.get(curWord)) {
+                dfs(nextWord, endWord, graph, path, result);
+            }
+        }
+
+        path.remove(path.size()-1);
+    }
 }
