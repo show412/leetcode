155e76bedaa1b8173afc0b9887cbeea90e1b3ba4
hwsun <hwsun@freewheel.tv>
2019-09-17 15:53:58 +0800

    maxSumArray greedy solution

--- problems/common/test.go
+++ problems/common/test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 /*
@@ -29,98 +28,29 @@ true
 true
 */
 func main() {
-	res := findLadders("hit", "cog", []string{"hot", "dot", "dog", "lot", "log", "cog"})
+	res := maxSubArray([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4})
 	fmt.Println(res)
 }
 
-func findLadders(beginWord string, endWord string, wordList []string) [][]string {
-	path := make([]string, 0)
-	result := make([][]string, 0)
-	graph := make(map[string][]string, 0)
-	dict := make(map[string]bool, 0)
-	for _, v := range wordList {
-		dict[v] = true
+func maxSubArray(nums []int) int {
+	if len(nums) == 0 {
+		return 0
 	}
-	buildGraph(beginWord, endWord, graph, dict)
-	dfs(beginWord, endWord, graph, &path, &result)
-	return result
-}
-
-func buildGraph(beginWord string, endWord string, graph map[string][]string, dict map[string]bool) {
-	visited := make(map[string]bool, 0)
-	toVisit := make(map[string]bool, 0)
-	queue := make([]string, 0)
-	queue = append(queue, beginWord)
-	toVisit[beginWord] = true
-	foundEnd := false
-	for len(queue) != 0 {
-		for k, v := range toVisit {
-			visited[k] = v
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	maxSum := nums[0]
+	max := nums[0]
+	for i := 1; i < len(nums); i++ {
+		num := nums[i]
+		if max <= 0 {
+			max = num
+		} else {
+			max += num
 		}
-		// visited = toVisit
-		toVisit = map[string]bool{}
-		count := len(queue)
-		for i := 0; i < count; i++ {
-			word := queue[0]
-			queue = queue[1:]
-			children := getNextLevel(word, dict)
-			for _, child := range children {
-				if child == endWord {
-					foundEnd = true
-				}
-				if _, ok := visited[child]; !ok {
-					if _, ok1 := graph[word]; !ok1 {
-						graph[word] = make([]string, 0)
-					}
-					graph[word] = append(graph[word], child)
-				}
-				// 防止重复寻找
-				_, ok2 := visited[child]
-				_, ok3 := toVisit[child]
-				if !ok2 && !ok3 {
-					queue = append(queue, child)
-					toVisit[child] = true
-				}
-			}
-		}
-		if foundEnd == true {
-			break
+		if max > maxSum {
+			maxSum = max
 		}
 	}
-	return
-}
-
-func dfs(curWord string, endWord string, graph map[string][]string, path *[]string, result *[][]string) {
-	*path = append(*path, curWord)
-	if curWord == endWord {
-		tmp := make([]string, len(*path))
-		copy(tmp, *path)
-		*result = append(*result, tmp)
-	} else if _, ok := graph[curWord]; ok {
-		for _, nextWord := range graph[curWord] {
-			dfs(nextWord, endWord, graph, path, result)
-		}
-	}
-	*path = (*path)[:len(*path)-1]
-	return
-}
-
-func getNextLevel(word string, dict map[string]bool) []string {
-	result := make([]string, 0)
-	wordArray := strings.Split(word, "")
-	for i := 0; i < len(wordArray); i++ {
-		tmp := make([]string, len(wordArray))
-		copy(tmp, wordArray)
-		for j := 'a'; j <= 'z'; j++ {
-			tmp[i] = string(j)
-			transWord := strings.Join(tmp, "")
-			if transWord == word {
-				continue
-			}
-			if _, ok := dict[transWord]; ok {
-				result = append(result, transWord)
-			}
-		}
-	}
-	return result
+	return maxSum
 }
