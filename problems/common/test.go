1e515c4ef1b30388cf66f0cc6d3a41c24171751d
hwsun <hwsun@freewheel.tv>
2019-09-21 13:30:59 +0800

    basic calculator solution

--- problems/common/test.go
+++ problems/common/test.go
@@ -2,110 +2,83 @@ package main
 
 import (
 	"fmt"
+	"unicode"
 )
 
 /*
 test case:
-4
-[[2,0],[1,0],[3,1],[3,2],[1,3]]
-false
+"1 + 1" = 2
+" 6-4 / 2 " = 4
+"2*(5+5*2)/3+(6/2+8)" = 21
+"(2+6* 3+5- (3*14/7+2)*5)+3"=-12
+"-1+4*3/3/3" 0
 */
 func main() {
-	res := canFinish(4, [][]int{[]int{2, 0}, []int{1, 0}, []int{3, 1}, []int{3, 2}, []int{1, 3}})
+	res := calculate("-1+4*3/3/3")
 	// res := canFinish(4, [][]int{[]int{0, 1}})
 	fmt.Println(res)
 }
 
-type Graph struct {
-	nodes []int         // 节点集
-	edges map[int][]int // 邻接表表示的无向图
-}
-
-func generateGraph(num int, edges [][]int) Graph {
-	g := Graph{nodes: make([]int, 0), edges: make(map[int][]int, 0)}
-	// generate node
-	for i := 0; i < num; i++ {
-		node := i
-		g.nodes = append(g.nodes, node)
+func calculate(s string) int {
+	l1, o1, l2, o2 := 0, 1, 1, 1
+	if s[0] == '-' {
+		s = "0" + s
 	}
-	// generate graph direct edges
-	for i := 0; i < len(edges); i++ {
-		sNode := edges[i][0]
-		eNode := edges[i][1]
-		g.edges[sNode] = append(g.edges[sNode], eNode)
-	}
-	return g
-}
-
-func checkCycle(g Graph, num int) bool {
-	// 不能用BFS算法 因为可能起点不是一个边的起点，所以用图的DFS算法
-	/*
-		即为一条道走到黑的搜索策略，行不通退回来换另外一条道再走到黑，依次直到搜索完成。
-		其过程简要来说是对每一个可能的分支路径深入到不能再深入为止，而且每个节点只能访问一次。
-	*/
-	visited := make(map[int]bool, 0)
-	visiting := make(map[int]bool, 0)
-	for i := 0; i < num; i++ {
-		// if visited[i] != true && len(g.edges[i]) > 0 {
-		if dfs(g, i, visited, visiting) == true {
-			return false
-		}
-		// }
-	}
-	return true
-}
-
-func dfs(g Graph, i int, visited map[int]bool, visiting map[int]bool) bool {
-	if visited[i] != true {
-		visited[i] = true
-		visiting[i] = true
-		for j := 0; j < len(g.edges[i]); j++ {
-			neighbour := g.edges[i][j]
-			if visited[neighbour] != true && dfs(g, neighbour, visited, visiting) {
-				fmt.Println("1111")
-				fmt.Println(visited)
-				fmt.Println(neighbour)
-				return true
-			} else if visiting[neighbour] == true {
-				fmt.Println("2222")
-				fmt.Println(visiting)
-				fmt.Println(neighbour)
-				return true
+	for i := 0; i < len(s); i++ {
+		c := rune(s[i])
+		if unicode.IsDigit(c) {
+			num := int(c - '0')
+			for (i+1) < len(s) && unicode.IsDigit(rune(s[i+1])) {
+				num = num*10 + int(rune(s[i+1])-'0')
+				i++
 			}
-		}
-	}
-	visiting[i] = false
-	return false
-}
 
-func canFinish(numCourses int, prerequisites [][]int) bool {
-	g := generateGraph(numCourses, prerequisites)
-	// check cycle
-	// visited := make(map[int]bool, numCourses)
-	fmt.Println(g)
-	res := checkCycle(g, numCourses)
-	return res
-}
-
-// 这是BFS遍历图的方法
-/*
-func checkCycle(g Graph) bool {
-
-	visited := make(map[int]bool, 0)
-	q := make([]int, 0)
-	q = append(q, g.nodes[0])
-	visited[g.nodes[0]] = true
-	for len(q) != 0 {
-		node := q[0]
-		q = q[1:]
-		visited[node] = true
-		for _, neighbour := range g.edges[node] {
-			if visited[neighbour] == true {
-				return false
+			if o2 == 1 {
+				l2 = l2 * num
+			} else {
+				l2 = l2 / num
 			}
-			q = append(q, neighbour)
+		} else if c == '(' {
+			j := i
+			for cnt := 0; i < len(s); i++ {
+				if s[i] == '(' {
+					cnt++
+				}
+				if s[i] == ')' {
+					cnt--
+				}
+				if cnt == 0 {
+					break
+				}
+			}
+			num := calculate(s[(j + 1):i])
+
+			if o2 == 1 {
+				l2 = l2 * num
+			} else {
+				l2 = l2 / num
+			}
+		} else if c == '*' || c == '/' {
+			if c == '*' {
+				o2 = 1
+			} else {
+				o2 = -1
+			}
+		} else if c == '+' || c == '-' {
+			l1 = l1 + o1*l2
+			if c == '+' {
+				o1 = 1
+			} else {
+				o1 = -1
+			}
+			l2 = 1
+			o2 = 1
 		}
+		fmt.Println("****")
+		fmt.Println(l1)
+		fmt.Println(o1)
+		fmt.Println(l2)
+		fmt.Println(o2)
 	}
-	return true
+	return (l1 + o1*l2)
 }
-*/
