be3c1892b866301c85e25c2d6f2e61d4a1c4563a
hwsun <hwsun@freewheel.tv>
2019-09-22 14:29:25 +0800

    most common word solution

--- problems/common/test.go
+++ problems/common/test.go
@@ -2,83 +2,59 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"unicode"
 )
 
 /*
 test case:
-"1 + 1" = 2
-" 6-4 / 2 " = 4
-"2*(5+5*2)/3+(6/2+8)" = 21
-"(2+6* 3+5- (3*14/7+2)*5)+3"=-12
-"-1+4*3/3/3" 0
+"a, a, a, a, b,b,b,c, c"
+["a"]
+"b"
+
+paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
+banned = ["hit"]
+Output: "ball"
 */
 func main() {
-	res := calculate("-1+4*3/3/3")
+	res := mostCommonWord("a, a, a, a, b,b,b,c, c", []string{"a"})
 	// res := canFinish(4, [][]int{[]int{0, 1}})
 	fmt.Println(res)
 }
 
-func calculate(s string) int {
-	l1, o1, l2, o2 := 0, 1, 1, 1
-	if s[0] == '-' {
-		s = "0" + s
+func mostCommonWord(paragraph string, banned []string) string {
+	bannedMap := make(map[string]bool, 0)
+	for _, v := range banned {
+		bannedMap[v] = true
 	}
-	for i := 0; i < len(s); i++ {
-		c := rune(s[i])
-		if unicode.IsDigit(c) {
-			num := int(c - '0')
-			for (i+1) < len(s) && unicode.IsDigit(rune(s[i+1])) {
-				num = num*10 + int(rune(s[i+1])-'0')
-				i++
-			}
-
-			if o2 == 1 {
-				l2 = l2 * num
-			} else {
-				l2 = l2 / num
-			}
-		} else if c == '(' {
-			j := i
-			for cnt := 0; i < len(s); i++ {
-				if s[i] == '(' {
-					cnt++
-				}
-				if s[i] == ')' {
-					cnt--
-				}
-				if cnt == 0 {
-					break
-				}
-			}
-			num := calculate(s[(j + 1):i])
-
-			if o2 == 1 {
-				l2 = l2 * num
-			} else {
-				l2 = l2 / num
-			}
-		} else if c == '*' || c == '/' {
-			if c == '*' {
-				o2 = 1
-			} else {
-				o2 = -1
-			}
-		} else if c == '+' || c == '-' {
-			l1 = l1 + o1*l2
-			if c == '+' {
-				o1 = 1
-			} else {
-				o1 = -1
-			}
-			l2 = 1
-			o2 = 1
+	pureText := make([]byte, 0)
+	for i := 0; i < len(paragraph); i++ {
+		letter := paragraph[i]
+		if unicode.IsLetter(rune(letter)) || unicode.IsSpace(rune(letter)) {
+			pureText = append(pureText, letter)
+		} else {
+			pureText = append(pureText, ' ')
 		}
-		fmt.Println("****")
-		fmt.Println(l1)
-		fmt.Println(o1)
-		fmt.Println(l2)
-		fmt.Println(o2)
 	}
-	return (l1 + o1*l2)
+	wordArray := strings.Split(strings.ToLower(string(pureText)), " ")
+	fmt.Println(wordArray)
+	wordMap := make(map[string]int, 0)
+	maxWord := ""
+	maxNum := 0
+	for i := 0; i < len(wordArray); i++ {
+		word := strings.TrimSpace(wordArray[i])
+
+		if bannedMap[word] == true || len(word) == 0 {
+			continue
+		}
+		if _, ok := wordMap[word]; !ok {
+			wordMap[word] = 0
+		}
+		wordMap[word] += 1
+		if wordMap[word] > maxNum {
+			maxWord = word
+			maxNum = wordMap[word]
+		}
+	}
+	return maxWord
 }
