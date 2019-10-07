7d2d3ddad47a49f87c404e0a1cbe05c8fc2e980d
hwsun <hwsun@freewheel.tv>
2019-10-07 18:32:47 +0800

    2sum 3sum 4sum solution

--- problems/facebook/medium/3sum.go
+++ problems/facebook/medium/3sum.go
@@ -2,7 +2,9 @@ import "sort"
 
 // https://leetcode.com/problems/3sum/
 /*
-Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+Given an array nums of n integers, are there elements a, b, c in nums
+such that a + b + c = 0?
+Find all unique triplets in the array which gives the sum of zero.
 
 Note:
 
