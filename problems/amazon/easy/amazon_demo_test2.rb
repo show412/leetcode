4ef537d30f8a1dad0d488543148ced780ddb4ef9
hwsun <hwsun@freewheel.tv>
2019-11-01 00:05:13 +0800

    amazon demo test 2

---
+++ problems/amazon/easy/amazon_demo_test2.rb
@@ -0,0 +1,9 @@
+# GCD 5 [2,4,6,8,10]
+# GCD 5 [2,3,4,5,6]
+def generalizedGCD(num, arr)
+    # WRITE YOUR CODE HERE
+end
+
+#  public int gcd(int a, int b) {
+#         return b > 0 ? gcd(b, a % b) : a;
+#     }
