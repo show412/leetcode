0828a7f8e3fde98978226b44155c7967cf0162bf
hwsun <hwsun@freewheel.tv>
2019-11-15 22:47:36 +0800

    implement trie prefix tree TBD

---
+++ problems/google/medium/implement-trie-prefix-tree.go
@@ -0,0 +1,49 @@
+// https://leetcode.com/problems/implement-trie-prefix-tree/
+/*
+Implement a trie with insert, search, and startsWith methods.
+
+Example:
+
+Trie trie = new Trie();
+
+trie.insert("apple");
+trie.search("apple");   // returns true
+trie.search("app");     // returns false
+trie.startsWith("app"); // returns true
+trie.insert("app");
+trie.search("app");     // returns true
+Note:
+
+You may assume that all inputs are consist of lowercase letters a-z.
+All inputs are guaranteed to be non-empty strings.
+*/
+type Trie struct {
+}
+
+/** Initialize your data structure here. */
+func Constructor() Trie {
+
+}
+
+/** Inserts a word into the trie. */
+func (this *Trie) Insert(word string) {
+
+}
+
+/** Returns if the word is in the trie. */
+func (this *Trie) Search(word string) bool {
+
+}
+
+/** Returns if there is any word in the trie that starts with the given prefix. */
+func (this *Trie) StartsWith(prefix string) bool {
+
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Insert(word);
+ * param_2 := obj.Search(word);
+ * param_3 := obj.StartsWith(prefix);
+ */
