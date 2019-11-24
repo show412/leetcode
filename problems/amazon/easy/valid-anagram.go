0b975a1abff4c4eafad53cad35aec8a6acac5f42
hwsun <hwsun@freewheel.tv>
2019-11-24 11:40:34 +0800

    valid-anagram solution

--- problems/amazon/easy/valid-anagram.go
+++ problems/amazon/easy/valid-anagram.go
@@ -17,5 +17,18 @@ Follow up:
 What if the inputs contain unicode characters? How would you adapt your solution to such case?
 */
 func isAnagram(s string, t string) bool {
-
+	if len(s) != len(t) {
+		return false
+	}
+	m := make([]int, 26)
+	for i := 0; i < len(s); i++ {
+		m[s[i]-'a']++
+	}
+	for j := 0; j < len(t); j++ {
+		m[t[j]-'a']--
+		if m[t[j]-'a'] < 0 {
+			return false
+		}
+	}
+	return true
 }
