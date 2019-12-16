8168bd628a061ec26fb06d3ca2a9707bfe31b2e3
hwsun <hwsun@freewheel.tv>
2019-12-16 17:43:44 +0800

    shortest distance in grid practice

--- practice/bfs/shortest_distance_in_grid.go
+++ practice/bfs/shortest_distance_in_grid.go
@@ -1,7 +1,9 @@
 import "math"
+
 // https://leetcode.com/problems/shortest-distance-from-all-buildings/
 // Share
-// You want to build a house on an empty land which reaches all buildings in the shortest amount of distance. You can only move up, down, left and right. You are given a 2D grid of values 0, 1 or 2, where:
+// You want to build a house on an empty land which reaches all buildings in the shortest amount of distance.
+// You can only move up, down, left and right. You are given a 2D grid of values 0, 1 or 2, where:
 
 // Each 0 marks an empty land which you can pass by freely.
 // Each 1 marks a building which you cannot pass through.
@@ -25,7 +27,7 @@ import "math"
 // There will be at least one building. If it is not possible to build such house according to the above rules, return -1.
 // https://leetcode.com/problems/shortest-distance-from-all-buildings/
 // 这种解法是在没有2的情况下 有了2就只能BFS了
-// O(n*m)
+// TC is O(n*m)
 // 我们可以借鉴的是通过两个数组记下哪些row和col是1，
 // 然后可以用另一个数组提前算好row方向的distance和col方向的distance
 // 按着算法 同一个row上的row距离是0 同理同一个col上的col距离是0
@@ -58,9 +60,6 @@ func shortestDistance(grid [][]int) int {
 				d := rowDistance[i] + colDistance[j]
 				if d >= 0 {
 					if res == -1 || res > d {
-						// fmt.Println(i)
-						// fmt.Println(j)
-						// fmt.Println(d)
 						res = d
 					}
 				}
@@ -89,86 +88,24 @@ func getDistance(size int, house []int, distance []int) {
   int nextRow = curr[0] + shift[k];
   int nextCol = curr[1] + shift[k + 1];
 */
-public class Solution {
-    public int shortestDistance(int[][] grid) {
-        if (grid == null || grid[0].length == 0) return 0;
-				// 上下左右利用这个数组 然后遍历这样的code去实现
-				// int nextRow = curr[0] + shift[k];
-        // int nextCol = curr[1] + shift[k + 1];
-				final int[] shift = new int[] {0, 1, 0, -1, 0};
-
-        int row  = grid.length, col = grid[0].length;
-        int[][] distance = new int[row][col];
-        int[][] reach = new int[row][col];
-        int buildingNum = 0;
-
-        for (int i = 0; i < row; i++) {
-            for (int j =0; j < col; j++) {
-                if (grid[i][j] == 1) {
-                    buildingNum++;
-                    Queue<int[]> myQueue = new LinkedList<int[]>();
-                    myQueue.offer(new int[] {i,j});
-
-                    boolean[][] isVisited = new boolean[row][col];
-                    int level = 1;
-
-                    while (!myQueue.isEmpty()) {
-                        int qSize = myQueue.size();
-                        for (int q = 0; q < qSize; q++) {
-                            int[] curr = myQueue.poll();
-
-                            for (int k = 0; k < 4; k++) {
-                                int nextRow = curr[0] + shift[k];
-                                int nextCol = curr[1] + shift[k + 1];
-
-                                if (nextRow >= 0 && nextRow < row && nextCol >= 0 && nextCol < col
-                                    && grid[nextRow][nextCol] == 0 && !isVisited[nextRow][nextCol]) {
-                                        //The shortest distance from [nextRow][nextCol] to this building
-                                        // is 'level'.
-                                        distance[nextRow][nextCol] += level;
-                                        reach[nextRow][nextCol]++;
-
-                                        isVisited[nextRow][nextCol] = true;
-                                        myQueue.offer(new int[] {nextRow, nextCol});
-                                    }
-                            }
-                        }
-                        level++;
-                    }
-                }
-            }
-        }
-				/*
-					distance[i][j] 不一定和reach[i][j]就是相等的
-					distance是记距离的 reach是记有多少个1可以到这
-					因为level是每次queue之后都会加1
-				*/
-        int shortest = Integer.MAX_VALUE;
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (grid[i][j] == 0 && reach[i][j] == buildingNum) {
-                    shortest = Math.min(shortest, distance[i][j]);
-                }
-            }
-        }
-
-        return shortest == Integer.MAX_VALUE ? -1 : shortest;
-
-
-    }
-}
 
 // this is typical BFS for a matrix
+// https://github.com/grandyang/leetcode/issues/317  解法2
+/*
+从每个[1]出发 找到可以 build 建筑物的点[0]
+*/
 func shortestDistance(grid [][]int) int {
 	if grid == nil || len(grid[0]) == 0 {
 		return -1
 	}
 	path := []int{0, 1, 0, -1, 0}
 	// distance := [len(grid)][len(grid[0])]int{}
+	// distance[i][j]代表 i,j到达所有建筑物点的距离
 	distance := make([][]int, len(grid))
 	for i := 0; i < len(grid); i++ {
 		distance[i] = make([]int, len(grid[0]))
 	}
+	// reach[i][j] 代表可以到达建筑物的数量
 	reach := make([][]int, len(grid))
 	for i := 0; i < len(grid); i++ {
 		reach[i] = make([]int, len(grid[0]))
@@ -207,9 +144,6 @@ func shortestDistance(grid [][]int) int {
 	}
 	const MAX = int(^uint(0) >> 1)
 	res := MAX
-	// fmt.Println(buildingNum)
-	// fmt.Println(distance)
-	// fmt.Println(reach)
 	for i := 0; i < len(grid); i++ {
 		for j := 0; j < len(grid[0]); j++ {
 			if grid[i][j] == 0 && reach[i][j] == buildingNum {
