fa3813ddb1ab0826acab29bf69cf1a2cddb775f8
show412 <show412@gmail.com>
2024-03-06 11:11:46 +0800

    stack+dfs

--- problems/google/medium/generateParenthesis.go
+++ problems/google/medium/generateParenthesis.go
@@ -1,4 +1,11 @@
-import "strconv"
+/*
+ * @Author: hongwei.sun
+ * @Date: 2021-01-22 18:45:52
+ * @LastEditors: your name
+ * @LastEditTime: 2024-03-06 11:10:26
+ * @Description: file content
+ */
+import "strings"
 
 // https://leetcode.com/problems/generate-parentheses/
 // Given n pairs of parentheses,
@@ -14,10 +21,12 @@ import "strconv"
 //   "()()()"
 // ]
 
-// solution of answer
-// because only the open bracket size is bigger than the close bracket size
-// it will be reseanable
-
+/* solution of answer
+three conditions to be reseanable
+1, when open bracket size less than n, add (
+2, when only the open bracket size is bigger than the close bracket size, can )
+3, when open bracket and clost bracket are both 3, one result generated
+*/
 func generateParenthesis(n int) []string {
 	if n == 0 {
 		return []string{}
@@ -33,6 +42,7 @@ func recursionAdd(n int, str string, open int, close int, res *[]string) {
 		return
 	}
 	// it seems a DFS recursion
+	// here str is almost one stack.
 	if open < n {
 		recursionAdd(n, str+"(", open+1, close, res)
 	}
@@ -40,6 +50,36 @@ func recursionAdd(n int, str string, open int, close int, res *[]string) {
 		recursionAdd(n, str+")", open, close+1, res)
 	}
 	return
+}
+
+// DFS actually it's almost same solution as above, just we use one stack
+
+func generateParenthesis(n int) []string {
+	if n == 0 {
+		return []string{}
+	}
+	stack := make([]string, 0)
+	result := make([]string, 0)
+
+	dfs(0, 0, n, &stack, &result)
+
+	return result
+}
+
+func dfs(openCount int, closeCount int, n int, stack *[]string, result *[]string) {
+	if openCount == n && closeCount == n {
+		*result = append(*result, strings.Join(*stack, ""))
+	}
+	if openCount < n {
+		*stack = append(*stack, "(")
+		dfs(openCount+1, closeCount, n, stack, result)
+		*stack = (*stack)[:len(*stack)-1]
+	}
+	if openCount > closeCount {
+		*stack = append(*stack, ")")
+		dfs(openCount, closeCount+1, n, stack, result)
+		*stack = (*stack)[:len(*stack)-1]
+	}
 }
 
 // the solution is like divided conquer
@@ -57,81 +97,4 @@ func generateParenthesis(n int) []string {
 		}
 	}
 	return res
-}
-
-// DFS like permutation
-// "(" means 1 ")" means -1
-// if sum >=0 it's valid
-// It's TLE
-func generateParenthesis(n int) []string {
-	if n == 0 {
-		return []string{}
-	}
-	nums := make([]int, 0)
-	visit := make(map[int]bool)
-	uniq := make(map[string]bool)
-	res := make([][]int, 0)
-	entry := make([]int, 0)
-	result := make([]string, 0)
-	for i := 0; i < n; i++ {
-		nums = append(nums, 1)
-		nums = append(nums, -1)
-	}
-	dfs(nums, visit, entry, &res, uniq)
-	for _, item := range res {
-		str := ""
-		for _, i := range item {
-			if i == 1 {
-				str += "("
-			} else if i == -1 {
-				str += ")"
-			}
-		}
-		result = append(result, str)
-	}
-	return result
-}
-
-func dfs(nums []int, visit map[int]bool, entry []int, res *[][]int, uniq map[string]bool) {
-	if len(entry) == len(nums) {
-		cpy := make([]int, len(entry))
-		copy(cpy, entry)
-		str := ""
-		for i := 0; i < len(cpy); i++ {
-			str += strconv.Itoa(cpy[i])
-		}
-		// fmt.Println(str)
-		if v, ok := uniq[str]; ok && v == true {
-			return
-		}
-		*res = append(*res, cpy)
-		uniq[str] = true
-		return
-	}
-	for i := 0; i < len(nums); i++ {
-		// if i != 0 && nums[i-1] == nums[i] {
-		// 	continue
-		// }
-		if v, ok := visit[i]; ok && v == true {
-			continue
-		}
-		entry = append(entry, nums[i])
-		visit[i] = true
-		if isValid(entry) == true {
-			dfs(nums, visit, entry, res, uniq)
-		}
-		entry = entry[:len(entry)-1]
-		visit[i] = false
-	}
-}
-
-func isValid(arr []int) bool {
-	cur := 0
-	for _, i := range arr {
-		cur += i
-		if cur < 0 {
-			return false
-		}
-	}
-	return true
 }
