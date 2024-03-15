f0b5a4ac6bbd26dc0eac2809bd0af8f5cfd606d2
show412 <show412@gmail.com>
2024-03-15 23:42:58 +0800

    encode-and-decode-strings

--- problems/google/TBD/medium/encode-and-decode-strings.go
+++ problems/google/TBD/medium/encode-and-decode-strings.go
@@ -2,7 +2,7 @@
  * @Author: hongwei.sun
  * @Date: 2021-01-22 18:45:52
  * @LastEditors: your name
- * @LastEditTime: 2024-03-14 23:26:52
+ * @LastEditTime: 2024-03-15 23:23:31
  * @Description: file content
  */
 // https://leetcode.com/problems/encode-and-decode-strings/
@@ -42,21 +42,45 @@ Do not rely on any library method such as eval or serialize methods. You should 
 this question is about how to make delimiter between string
 we can define encode is like  {len(string)}#string
 we can define decode to parse this encoded string to work out []string
+
+this question is about int<->string<->rune convert
 */
-type Codec struct {
-  var codec Codec
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	s := Encode([]string{"fda", "12dc#y", "pesm&", "mvn3ws"})
+	fmt.Println(s)
+	sd := Decode(s)
+	fmt.Println(sd)
 }
 
-// Encodes a list of strings to a single string.
-func (codec *Codec) Encode(strs []string) string {
-
+func Encode(strs []string) string {
+	var res string
+	for _, str := range strs {
+		tmp := strconv.Itoa(len(str)) + "#" + str
+		res += tmp
+	}
+	return res
 }
 
-// Decodes a single string to a list of strings.
-func (codec *Codec) Decode(strs string) []string {
-
+func Decode(strs string) []string {
+	res := make([]string, 0)
+	runeArray := []rune(strs)
+	for i := 1; i < len(runeArray); i++ {
+		l := 0
+		s := string(runeArray[i])
+		if s == "#" {
+			l = int(runeArray[i-1] - '0')
+			res = append(res, string(runeArray[(i+1):(i+1+l)]))
+			i += l
+		}
+	}
+	return res
 }
-
-// Your Codec object will be instantiated and called as such:
-// var codec Codec
-// codec.Decode(codec.Encode(strs));
