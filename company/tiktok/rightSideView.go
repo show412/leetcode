621837293cbc2c38965ad2f8f6e9fd3ce34143d0
show412 <show412@gmail.com>
2024-04-11 16:17:47 +0800

    binary tree right side view

--- company/tiktok/rightSideView.go
+++ company/tiktok/rightSideView.go
@@ -1,8 +1,8 @@
 /*
  * @Author: hongwei.sun
- * @Date: 2021-01-22 18:45:51
+ * @Date: 2024-04-11 16:16:59
  * @LastEditors: hongwei.sun
- * @LastEditTime: 2024-04-11 16:17:14
+ * @LastEditTime: 2024-04-11 16:16:59
  * @Description: file content
  */
 // https://leetcode.com/problems/binary-tree-right-side-view/
@@ -46,6 +46,7 @@ func rightSideView(root *TreeNode) []int {
 	queue = append(queue, root)
 	for len(queue) != 0 {
 		size := len(queue)
+		value := 0
 		for i := 0; i < size; i++ {
 			node := queue[0]
 			if size > 1 {
@@ -53,7 +54,7 @@ func rightSideView(root *TreeNode) []int {
 			} else {
 				queue = []*TreeNode{}
 			}
-			value := node.Val
+			value = node.Val
 			if i == size-1 {
 				res = append(res, value)
 			}
