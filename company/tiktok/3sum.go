681d99a88c6f5ab4228a222abea8b1becce693ff
show412 <show412@gmail.com>
2024-04-17 12:31:11 +0800

    practice tiktok

---
+++ company/tiktok/3sum.go
@@ -0,0 +1,75 @@
+/*
+ * @Author: hongwei.sun
+ * @Date: 2024-04-17 10:26:45
+ * @LastEditors: hongwei.sun
+ * @LastEditTime: 2024-04-17 10:26:46
+ * @Description: file content
+ */
+import "sort"
+
+// https://leetcode.com/problems/3sum/
+/*
+Given an array nums of n integers, are there elements a, b, c in nums
+such that a + b + c = 0?
+Find all unique triplets in the array which gives the sum of zero.
+
+Note:
+
+The solution set must not contain duplicate triplets.
+
+Example:
+
+Given array nums = [-1, 0, 1, 2, -1, -4],
+
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+*/
+/*
+input: [-2,0,3,-1,4,0,3,4,1,1,1,-3,-5,4,0]
+output: [[-5,1,4],[-3,-1,4],[-3,0,3],[-2,-1,3],[-2,1,1],[-1,0,1],[0,0,0]]
+*/
+/*
+1, sort
+2, from negative value 
+3, 2sum to find other 2 values
+4, find the other solution with other combination
+这题关键是去重 要用nums[i] == nums[i-1] 和前面的去比较 然后continue
+因为如果i 和 i-1相等 那就是会在后面双指针的时候找到和i-1一样的 就重复了
+2sum也是一样的， 所以要和前面的i-1比
+*/
+func threeSum(nums []int) [][]int {
+	res := make([][]int, 0)
+	sort.Ints(nums)
+	for i := 0; i < len(nums)-2; i++ {
+		if nums[i] > 0 {
+			break
+		}
+		if i > 0 && nums[i-1] == nums[i] {
+			continue
+		}
+		l := i + 1
+		r := len(nums) - 1
+		// two sum to find other 2 values
+		target := 0 - nums[i]
+		for l < r {
+			sum := nums[l] + nums[r]
+			if sum > target {
+				r--
+			} else if sum < target {
+				l++
+			} else {
+				res = append(res, []int{nums[i], nums[l], nums[r]})
+				// if there are other combination, cause it's sorted, move l forward
+				// move r is same, just we want to move duplicated value
+				l++
+				for nums[l] == nums[l-1] && l < r {
+					l++
+				}
+			}
+		}
+	}
+	return res
+}
---
+++ company/tiktok/construct-binary-tree-from-preorder-and-inorder-traversal.go
@@ -0,0 +1,71 @@
+/*
+ * @Author: hongwei.sun
+ * @Date: 2024-04-17 10:43:51
+ * @LastEditors: hongwei.sun
+ * @LastEditTime: 2024-04-17 12:29:53
+ * @Description: file content
+ */
+// https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
+/*
+Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+ 
+
+Example 1:
+
+
+Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+Output: [3,9,20,null,null,15,7]
+Example 2:
+
+Input: preorder = [-1], inorder = [-1]
+Output: [-1]
+ 
+
+Constraints:
+
+1 <= preorder.length <= 3000
+inorder.length == preorder.length
+-3000 <= preorder[i], inorder[i] <= 3000
+preorder and inorder consist of unique values.
+Each value of inorder also appears in preorder.
+preorder is guaranteed to be the preorder traversal of the tree.
+inorder is guaranteed to be the inorder traversal of the tree.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+/*
+1, 从preorder能知道root
+2，从inorder能知道root的位置，进而知道左子树有多少个节点，右子树有多少个节点
+3，然后分别递归得到左右子树
+*/
+ func buildTree(preorder []int, inorder []int) *TreeNode {
+    if len(preorder) == 0 || len(inorder) == 0 {
+		return nil
+	}
+	root := &TreeNode{Val:preorder[0]}
+	var rootIndex int
+	for i := 0; i < len(inorder); i++ {
+		if inorder[i] == preorder[0] {
+			rootIndex = i
+			break
+		}
+	}
+	/*
+	by rootIndex we can know how many left nodes and how many right nodes
+	extract left node numbers and right node numbers from preorder and inorder repectively
+	then recusion to build tree from left and right tree
+	rootIndex also means how many nodes is on left of root in inorder array
+	so we extract such numbers from preorder to preorder as left sub tree
+	*/
+	root.Left = buildTree(preorder[1:rootIndex+1], inorder[:rootIndex])
+	root.Right = buildTree(preorder[rootIndex+1:], inorder[rootIndex+1:])
+	return root
+ }
